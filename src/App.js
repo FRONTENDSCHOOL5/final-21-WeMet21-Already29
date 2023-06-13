4d5f8d233b91046ca5a63ff3136cc2f279448952
yonainthefish <yonaisgood@gmail.com>
2023-06-13 13:42:34 +0900

    style: profile UI 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import SignUp from "./pages/Profile";
+import { createGlobalStyle } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import reset from "styled-reset";
+import GlobalStyle from "./style/GlobalStyle"
+
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Profile />
+    </BrowserRouter>
   );
 }
 export default App;

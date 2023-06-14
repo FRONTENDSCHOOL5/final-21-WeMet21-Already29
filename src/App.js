713dfe2ccf6245501b40120be1d44e1576d273e1
kyeongboo-coder <kyeong0914@gmail.com>
2023-06-14 15:36:44 +0900

    chore : 스타일 컴포넘트 파일로 따로 분리하는 추가작업 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import Login from "./pages/Login/Login";
-import { createGlobalStyle } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
-import reset from "styled-reset";
-
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-:root{
-  --main-color : #058b2e
-}
-.a11y-hidden {
-    clip: rect(1px, 1px, 1px, 1px);
-    clip-path: inset(50%);
-    width: 1px;
-    height: 1px;
-    margin: -1px;
-    overflow: hidden;
-    padding: 0;
-    position: absolute;
-  }
-`;
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (

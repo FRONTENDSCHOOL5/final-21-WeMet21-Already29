902234191f047d62932dcbf1cf05692ff63997d3
beomkwan.choi <3031011@naver.com>
2023-06-14 22:32:13 +0900

    docs: browser router 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import Profile from "./pages/Profile/Profile";
+import GlobalStyle from "./style/GlobalStyle";
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Profile />
+      </BrowserRouter>
+    </>
   );
 }
 export default App;

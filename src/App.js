90d52cf8b9b60f9a272e4120a5c7ec7c3672a880
hansanghun <rkdla666@naver.com>
2023-06-14 20:38:25 +0900

    fix : GlobalStyle 파일 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import GlobalStyle from "./style/GlobalStyle";
 import Router from "./routes/Router";
-
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-  :root{
-    --main-color: rgba(5, 139, 46, 1);
-  }
-
-  #root{
-    width: min(80vw, 500px);
-    margin: 0 auto;
-  }
-
-  .a11y-hidden {
-    clip: rect(1px, 1px, 1px, 1px);
-    clip-path: inset(50%);
-    width: 1px;
-    height: 1px;
-    margin: -1px;
-    overflow: hidden;
-    padding: 0;
-    position: absolute;
-  }
-`;
+import Theme from './style/Theme';
+import { ThemeProvider } from 'styled-components';
+import { AuthContextProvider } from './contexts/Auth';
 
 function App() {
   return (
     <>
-        <GlobalStyle />
+      <AuthContextProvider>
+        <ThemeProvider theme={Theme}>
+          <GlobalStyle />
           <Router />
+        </ThemeProvider>
+      </AuthContextProvider> 
     </>
   );
 }

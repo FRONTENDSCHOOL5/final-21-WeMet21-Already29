74a53ece9f0e53d4178c854ab30dc14b82f9c69c
beomkwan.choi <3031011@naver.com>
2023-06-15 11:26:18 +0900

    docs: style 컴포넌트 분리

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,124 +1,9 @@
-import React, { Children, useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Products from "../../components/Products";
 import Loading from "../../components/Loading";
-import { Link, NavLink } from "react-router-dom";
-import styled from "styled-components";
+
 import share from "../../assets/images/share.png";
-import { GreenBigButton, GreenMdButton, GreenSmButton } from "../../components/Button/Button";
-
-const ProfileSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  gap: 1rem;
-  padding: 30px 0 26px;
-  margin-bottom: 6px;
-  border: 1px solid var(--line-gray-color);
-`;
-const ProfileHeader = styled.div`
-  display: flex;
-  align-items: center;
-
-  p {
-    color: var(--gray-color);
-  }
-
-  img {
-    width: 110px;
-    height: 110px;
-    margin: 0 4rem;
-  }
-`;
-
-const FollowCountSpan = styled.span`
-  display: block;
-  font-size: 1.8rem;
-  font-weight: bold;
-  color: var(--font-black-color);
-`;
-
-const ProfileIntro = styled.div`
-  .user-name {
-    font-size: 1.6rem;
-    font-weight: bold;
-  }
-
-  .account-name {
-    font-size: 1.2rem;
-    color: var(--gray-color);
-    margin: 10px 0 20px;
-  }
-
-  .intro {
-    color: var(--gray-color);
-    font-size: 1.4rem;
-  }
-`;
-
-const ShareButton = styled.button`
-  border-radius: 50%;
-  border: 1px solid var(--line-gray-color);
-  width: 34px;
-  height: 34px;
-  padding: 0;
-  vertical-align: top;
-
-  img {
-    width: 20px;
-    height: 20px;
-  }
-`;
-
-const LinkStyle = styled(NavLink)`
-  font-weight: bold;
-  font-size: 1.6rem;
-
-  &::after {
-    content: ">";
-    margin-left: 5px;
-  }
-`;
-
-const ProductUl = styled.ul`
-  width: max-content;
-  margin-top: 16px;
-
-  li {
-    display: inline-block;
-    width: 140px;
-  }
-
-  li + li {
-    margin: 0 10px;
-  }
-
-  .product-img {
-    border-radius: 10px;
-    object-fit: contain;
-    aspect-ratio: 1/1;
-  }
-
-  .product-title {
-    margin: 5px 0;
-    font-size: 1.4rem;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-
-  .product-price {
-    color: var(--main-color);
-    font-weight: bold;
-    font-size: 1.2rem;
-  }
-`;
-
-const ProductSection = styled.section`
-  overflow: hidden;
-  border: 1px solid var(--line-gray-color);
-  padding: 16px 0 23px 30px;
-`;
+import { GreenMdButton } from "../../components/Button/Button";
 
 export default function Profile() {
   const userAccountName = "testtestabc";
@@ -140,6 +25,7 @@ export default function Profile() {
   };
 
   const followButtonHandler = () => {
+    console.log("submit");
     fetch(`https://api.mandarin.weniv.co.kr/profile/${userData.accountname}/follow`, {
       method: "POST",
       headers: {
@@ -198,7 +84,7 @@ export default function Profile() {
               <p className="intro">{userData.intro ? userData.intro : "소개글이 작성되지 않았습니다"}</p>
             </ProfileIntro>
             <div className="profile-navbar">
-              <GreenMdButton contents={userData.isfollow ? "언팔로우" : "팔로우"} type="button" onClick={followButtonHandler}></GreenMdButton>
+              <GreenMdButton type="button" onClick={followButtonHandler} contents={userData.isfollow ? "언팔로우" : "팔로우"}></GreenMdButton>
               <ShareButton type="button">
                 <img src={share} alt="공유하기" />
               </ShareButton>
--- src/pages/Profile/ProfileStyle.jsx
+++ src/pages/Profile/ProfileStyle.jsx
@@ -1,2 +1,117 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+
+export const ProfileSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  gap: 1rem;
+  padding: 30px 0 26px;
+  margin-bottom: 6px;
+  border: 1px solid var(--line-gray-color);
+`;
+
+export const ProfileHeader = styled.div`
+  display: flex;
+  align-items: center;
+
+  p {
+    color: var(--gray-color);
+  }
+
+  img {
+    width: 110px;
+    height: 110px;
+    margin: 0 4rem;
+  }
+`;
+
+export const FollowCountSpan = styled.span`
+  display: block;
+  font-size: 1.8rem;
+  font-weight: bold;
+  color: var(--font-black-color);
+`;
+
+export const ProfileIntro = styled.div`
+  .user-name {
+    font-size: 1.6rem;
+    font-weight: bold;
+  }
+
+  .account-name {
+    font-size: 1.2rem;
+    color: var(--gray-color);
+    margin: 10px 0 20px;
+  }
+
+  .intro {
+    color: var(--gray-color);
+    font-size: 1.4rem;
+  }
+`;
+
+export const ShareButton = styled.button`
+  border-radius: 50%;
+  border: 1px solid var(--line-gray-color);
+  width: 34px;
+  height: 34px;
+  padding: 0;
+  vertical-align: top;
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+export const LinkStyle = styled(NavLink)`
+  font-weight: bold;
+  font-size: 1.6rem;
+
+  &::after {
+    content: ">";
+    margin-left: 5px;
+  }
+`;
+
+export const ProductUl = styled.ul`
+  width: max-content;
+  margin-top: 16px;
+
+  li {
+    display: inline-block;
+    width: 140px;
+  }
+
+  li + li {
+    margin: 0 10px;
+  }
+
+  .product-img {
+    border-radius: 10px;
+    object-fit: contain;
+    aspect-ratio: 1/1;
+  }
+
+  .product-title {
+    margin: 5px 0;
+    font-size: 1.4rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .product-price {
+    color: var(--main-color);
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+`;
+
+export const ProductSection = styled.section`
+  overflow: hidden;
+  border: 1px solid var(--line-gray-color);
+  padding: 16px 0 23px 30px;
+`;

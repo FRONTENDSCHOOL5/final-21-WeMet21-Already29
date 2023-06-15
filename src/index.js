f72d59013c9f5eb8464d128255184560acff5345
SangHun Han <93968241+Skyllerrr@users.noreply.github.com>
2023-06-16 01:08:07 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import StartStyle from './style/StartStyle';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-    <BrowserRouter>
-      <StartStyle />
-      <App />
-    </BrowserRouter>
-);
+root.render(<App />);

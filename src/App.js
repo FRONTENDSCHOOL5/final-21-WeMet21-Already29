101a356d0ea522b21fac676a4c3d49951a625bc4
beomkwan.choi <3031011@naver.com>
2023-06-15 17:24:58 +0900

    fix: App.js 초기세팅으로 변경

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
-import UploadProduct from "./pages/UploadProduct/UploadProduct";
-import ProductDetail from "./pages/ProductDetail/ProductDetail";
-import ProductList from "./pages/ProductList/ProductList";
 import GlobalStyle from "./style/GlobalStyle";
+import { BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <BrowserRouter>
-        <Link to="/uploadProduct">상품 올리기</Link>
-        <Link to="/productlist">상품 리스트 불러오기</Link>
-        <Routes>
-          <Route path="/uploadProduct" element={<UploadProduct />}></Route>
-          <Route path="/productlist" element={<ProductList />}></Route>
-          <Route path="/product/*" element={<Outlet />}>
-            <Route path="detail/:id" element={<ProductDetail />}></Route>
-            <Route path="modify/:id" element={<UploadProduct />}></Route>
-          </Route>
-        </Routes>
+        <Routes></Routes>
       </BrowserRouter>
     </>
   );

df8e3dcc721046fa26e22a9930df924b78339a0b
yonainthefish <yonaisgood@gmail.com>
2023-06-15 11:32:50 +0900

    style: 폰트 크기 변경, button 기본 설정 추가

--- src/style/GlobalStyle.jsx
+++ src/style/GlobalStyle.jsx
@@ -3,13 +3,13 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-
+  
   @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap');
-
+  
   body {
     font-family: 'Noto Sans KR', sans-serif;
   }
-
+  
   :root {
     --main-color: #058B2E; 
     --unactive-color: rgba(5, 139, 46, 0.4); //비활성화컬러
@@ -19,9 +19,9 @@ const GlobalStyle = createGlobalStyle`
     --font-red-color:#EB5757;
     --font-black-color: #0B0B0B;
 
-    --font-lg-size: 1.5rem; //24px 
-    --font-md-size: 0.875rem; //14px 
-    --font-sm-size: 0.75rem; //12px 
+    --font-lg-size: 2.4rem; 
+    --font-md-size: 1.4rem;  
+    --font-sm-size: 1.2rem; 
 
     --font-Bold: 700;
     --font-Medium: 500;
@@ -34,6 +34,22 @@ const GlobalStyle = createGlobalStyle`
     height: 100vh;
   }
 
+  img{
+    max-width: 100%;
+    vertical-align: top;
+  }
+
+  a{
+    text-decoration: none;
+    color: initial;
+  }
+
+  button {
+    all: unset;
+    cursor: pointer;
+  }
+
+  //텍스트 숨김 처리
   .a11y-hidden {
     clip: rect(1px, 1px, 1px, 1px);
     clip-path: inset(50%);
@@ -45,15 +61,6 @@ const GlobalStyle = createGlobalStyle`
     position: absolute;
   }
 
-  img{
-    max-width: 100%;
-    vertical-align: top;
-  }
-
-  a{
-    text-decoration: none;
-    color: initial;
-  }
 `;
 
 export default GlobalStyle;

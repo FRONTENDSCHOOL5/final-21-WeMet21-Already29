e301a2a81bfea5dfafca45fed160739d4bb5b43e
SangHun Han <93968241+Skyllerrr@users.noreply.github.com>
2023-06-16 01:09:12 +0900

    Update GlobalStyle.jsx

--- src/style/GlobalStyle.jsx
+++ src/style/GlobalStyle.jsx
@@ -6,6 +6,10 @@ const GlobalStyle = createGlobalStyle`
 
   @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap');
 
+  html {
+    font-size: 10px;
+  }
+
   body {
     font-family: 'Noto Sans KR', sans-serif;
   }

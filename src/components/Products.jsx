6fb67320960e0da72c755a13c43444f458d1915c
beomkwan.choi <3031011@naver.com>
2023-06-15 15:16:14 +0900

    feat: 상품 업로드 후 지난 시간 구하는 로직 구현

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -31,42 +31,43 @@ export default function Products(props) {
   const nowDateArray = nowKoreaTime.split("T")[0].split("-");
   const nowTimeArray = nowKoreaTime.split("T")[1].slice(0, 8).split(":");
 
+  const uploadDateCalculate = (updateDateArray, updateTimeArray) => {
+    // 같은 년, 월, 일에 업데이트 되었는지 확인
+    if (nowDateArray[0] === updateDateArray[0] && nowDateArray[1] === updateDateArray[1] && nowDateArray[2] === updateDateArray[2]) {
+      // 같은 년, 월, 일에 업데이트 되었다면 시간, 분이 같은지 확인
+      if (nowTimeArray[0] === updateTimeArray[0] && nowTimeArray[1] === updateTimeArray[1]) {
+        // 같다면 몇초전인지 출력
+        return nowTimeArray[2] - updateTimeArray[2] + "초 전";
+        // 시간이 같은지 확인
+      } else if (nowTimeArray[0] === updateTimeArray[0]) {
+        // 같다면 몇분전인지 출력
+        return nowTimeArray[1] - updateTimeArray[1] + "분 전";
+      } else {
+        // 모두 다르면 몇시간 전인지 출력
+        return nowTimeArray[0] - updateTimeArray[0] + "시간 전";
+      }
+      // 같은 년, 월에 업데이트 되었는지 확인
+    } else if (nowDateArray[0] === updateDateArray[0] && nowDateArray[1] === updateDateArray[1]) {
+      // 같은 년, 월에 업데이트 되었다면 몇일전인지 출력
+      return nowDateArray[2] - updateDateArray[2] + "일 전";
+      // 같은 년에 업데이트 되었는지 확인
+    } else if (nowDateArray[0] === updateDateArray[0]) {
+      // 같은 년에 업데이트 되었다면 몇달전인지 출력
+      return nowDateArray[1] - updateDateArray[1] + "달 전";
+    } else {
+      // 모든 조건이 부합하지 않으면 몇년전인지 출력
+      return nowDateArray[0] - updateDateArray[0] + "년 전";
+    }
+  };
+
   return (
     <>
       {productDatas ? (
         productDatas.map((item) => {
-          let when;
           // 유저가 게시글을 업데이트 한 [년, 월, 일]순으로 배열에 저장됨
           const updateDateArray = item.updatedAt.split("T")[0].split("-");
           // 유저가 게시글을 업데이트 한 [시, 분, 초]순으로 배열에 저장됨
           const updateTimeArray = item.updatedAt.split("T")[1].slice(0, 8).split(":");
-
-          // 같은 년, 월, 일에 업데이트 되었는지 확인
-          if (nowDateArray[0] === updateDateArray[0] && nowDateArray[1] === updateDateArray[1] && nowDateArray[2] === updateDateArray[2]) {
-            // 같은 년, 월, 일에 업데이트 되었다면 시간, 분이 같은지 확인
-            if (nowTimeArray[0] === updateTimeArray[0] && nowTimeArray[1] === updateTimeArray[1]) {
-              // 같다면 몇초전인지 출력
-              when = nowTimeArray[2] - updateTimeArray[2] + "초 전";
-              // 시간이 같은지 확인
-            } else if (nowTimeArray[0] === updateTimeArray[0]) {
-              // 같다면 몇분전인지 출력
-              when = nowTimeArray[1] - updateTimeArray[1] + "분 전";
-            } else {
-              // 모두 다르면 몇시간 전인지 출력
-              when = nowTimeArray[0] - updateDateArray[0] + "시간 전";
-            }
-            // 같은 년, 월에 업데이트 되었는지 확인
-          } else if (nowDateArray[0] === updateDateArray[0] && nowDateArray[1] === updateDateArray[1]) {
-            // 같은 년, 월에 업데이트 되었다면 몇일전인지 출력
-            when = nowDateArray[2] - updateDateArray[2] + "일 전";
-            // 같은 년에 업데이트 되었는지 확인
-          } else if (nowDateArray[0] === updateDateArray[0]) {
-            // 같은 년에 업데이트 되었다면 몇달전인지 출력
-            when = nowDateArray[1] - updateDateArray[1] + "달 전";
-          } else {
-            // 모든 조건이 부합하지 않으면 몇년전인지 출력
-            when = nowDateArray[0] - updateDateArray[0] + "년 전";
-          }
 
           return (
             <li key={item.id}>
@@ -78,7 +79,7 @@ export default function Products(props) {
                   <h3 className="product-title">{item.itemName}</h3>
                   <p className="product-price">{new Intl.NumberFormat().format(item.price)}원</p>
                 </div>
-                <span>{when}</span>
+                <span>{uploadDateCalculate(updateDateArray, updateTimeArray)}</span>
               </Link>
             </li>
           );

184fd595fc66bea8b253d9782b2489ad43335c37
beomkwan.choi <3031011@naver.com>
2023-06-17 00:47:15 +0900

    style: productDatas가 없을 시 로딩 컴포넌트 -> 빈 문자열 출력하게 수정

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Loading from "./Loading";
 import uploadDateCalculate from "../utils/uploadDateCalculate";
 
 export default function Products(props) {

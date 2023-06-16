3c4d156e28c7949670cb1b463c144327e5eb8d96
yonainthefish <yonaisgood@gmail.com>
2023-06-17 01:20:52 +0900

    feat: 이미지&게시글 업로드 api 연결 완료

--- src/pages/PostUpload/PostUpload.jsx
+++ src/pages/PostUpload/PostUpload.jsx
@@ -1,103 +1,86 @@
 import React, { useState } from "react";
 import Header from "../../components/HeaderMenu/HeaderMenu";
 import { Upload, Form, UploadInput, Img, Label } from "./PostUploadStyle";
-
 import profileImg from "../../assets/images/profileImg.svg";
 import uploadFile from "../../assets/images/uploadFile.svg";
 
 export default function PostUpload() {
-  const [contentText, setContentText] = useState('');
-  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [imagePost, setImagePost] = useState("");
+  const [postContent, setPostContent] = useState("");
 
-  const handleTextChange = (event) => {
-    setContentText(event.target.value);
+  const handleImageInput = async (event) => {
+    const formData = new FormData();
+    formData.append("file", event.target.files[0]);
+
+    try {
+      const response = await fetch("https://api.mandarin.weniv.co.kr/image/uploadfile", {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+      setImagePost("https://api.mandarin.weniv.co.kr/" + data.filename);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const handleFileChange = (event) => {
-    const files = event.target.files;
-    setSelectedFiles(Array.from(files));
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const url = 'https://mandarin.api.weniv.co.kr/post';
-    const token = localStorage.getItem('Token');
-
-    if (selectedFiles.length > 3) {
-      alert('이미지 파일은 최대 3장까지만 가능합니다.');
+  const handlePostUpload = async () => {
+    if (!postContent || !imagePost) {
+      console.log("내용 또는 이미지를 입력해주세요.");
       return;
     }
 
-    const imgUrls = [];
-    for (const file of selectedFiles) {
-      const imgUrl = await uploadImage(file);
-      imgUrls.push(imgUrl);
-    }
-
-    const requestData = {
+    const formData = JSON.stringify({
       post: {
-        content: contentText,
-        image: imgUrls.join(',')
-      }
-    };
+        content: postContent,
+        image: imagePost,
+      },
+    });
 
+    const token = localStorage.getItem("token");
+
+    console.log(token);
     try {
-      const response = await fetch(url, {
-        method: 'POST',
+      const response = await fetch("https://api.mandarin.weniv.co.kr/post", {
+        method: "POST",
         headers: {
-          "Authorization": `Bearer ${token}`,
-          "Content-Type": "application/json"
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify(requestData)
+        body: formData,
       });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Post created:', data.post);
-        // 여기에서 게시글 작성 후의 처리를 수행할 수 있습니다.
-        // 예를 들어, 페이지 이동이나 상태 업데이트 등을 수행할 수 있습니다.
-      } else {
-        console.error('Failed to create post:', response.status);
-      }
+      const data = await response.json();
+      console.log(data);
     } catch (error) {
-      console.error('Error creating post:', error);
+      console.log(error);
     }
   };
 
-  const uploadImage = async (file) => {
-    // 이미지를 업로드하는 로직을 구현해야 합니다.
-    // 이 부분은 이미지 업로드를 위한 API 호출 등으로 구현할 수 있습니다.
-    // 예를 들어, FormData를 사용하여 이미지를 서버로 전송하는 방식을 사용할 수 있습니다.
-    // 실제 구현 방법은 API 사양과 서버 요구 사항에 따라 달라질 수 있습니다.
-    // 이 예시에서는 임시적으로 이미지 파일의 이름을 반환하는 방식으로 구현했습니다.
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const fileName = file.name;
-        resolve(fileName);
-      }, 1000);
-    });
+  const handleContentChange = (event) => {
+    setPostContent(event.target.value);
   };
-
-
 
   return (
     <>
       <Header />
       <Upload>
-        <h2 className='a11y-hidden'>게시글작성</h2>
-        <Form method='post' onSubmit={handleSubmit}>
+        <h2 className='a11y-hidden'>게시글 작성</h2>
+        <Form method='post'>
           <Img src={profileImg} alt='profileImg' />
           <label htmlFor='txt-sync' className='a11y-hidden'>
             게시글 입력창입니다.
           </label>
-          <textarea id='txt-sync' cols='40' rows='6' maxLength='140' placeholder='게시글 입력하기...' className='upload-txt' ></textarea>
-          <Label htmlFor='file-sync' className='file-sync' >  
-            <img src={uploadFile} alt="uploadFile" />
+          <textarea id='txt-sync' cols='40' rows='6' maxLength='140' placeholder='게시글 입력하기...' className='upload-txt' value={postContent} onChange={handleContentChange}></textarea>
+          <Label htmlFor='file-sync' className='file-sync'>
+            <img src={uploadFile} alt='uploadFile' />
           </Label>
-          <UploadInput type='file' id='file-sync' accept='.png, .jpg, .jpeg' multiple hidden/>
+          <UploadInput type='file' id='file-sync' accept='.png, .jpg, .jpeg' multiple onChange={handleImageInput} hidden />
         </Form>
-        <div className='img-container'></div>
+        <div className='img-container'>{imagePost && <img src={imagePost} alt='Uploaded' />}</div>
+        <button type='button' onClick={handlePostUpload}>
+          게시글 업로드
+        </button>
       </Upload>
     </>
   );

c081d1566637eceeca29a057009bf69e025229a8
beomkwan.choi <3031011@naver.com>
2023-06-18 21:56:50 +0900

    chore: 사용하지 않는 useRef 제거

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,16 +1,14 @@
 import Products from "../../components/Products";
-import { useRef } from "react";
 import { Ul } from "./ProductListStyle";
 import { useParams } from "react-router-dom";
 
 export default function ProductList() {
   const userAccountName = useParams();
-  const slideUl = useRef(null);
 
   return (
     <section>
-      <Ul ref={slideUl}>
-        <Products userAccountName={userAccountName.id} />
+      <Ul>
+        <Products timeView={true} userAccountName={userAccountName.id} />
       </Ul>
     </section>
   );

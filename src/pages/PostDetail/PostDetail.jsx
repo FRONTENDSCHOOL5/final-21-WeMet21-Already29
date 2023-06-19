508aae61742792801fa33a117fcd4afb06ebc5e9
kyeongboo-coder <kyeong0914@gmail.com>
2023-06-20 07:57:05 +0900

    feat : post 상세 댓글 기능 추가

--- src/pages/PostDetail/PostDetail.jsx
+++ src/pages/PostDetail/PostDetail.jsx
@@ -1,118 +1,74 @@
-import React, { useState, useEffect } from "react";
-
-import Header from "../../components/Header/HeaderMenu/HeaderMenu";
-
-import { Upload, Form, UploadInput, Label, Textarea } from "./PostDetailStyle";
+import React, { useState } from "react";
+import profileImg from "../../assets/images/profileImg.png";
+import { Upload, Form, UploadInput, Label, Img } from "./PostDetailStyle";
 
 export default function PostDetail() {
-  // const [imagePath, setImagePath] = useState("");
-  const [post, setPost] = useState("");
-  const [image, setImage] = useState(null); // 추후에 다중 이미지로 작업할 때는 image를 배열로!
-  const [previewImage, setPreviewImage] = useState(null);
-  // const token = localStorage.getItem("token");
-  // console.log(token);
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGQ2ZWYzYjJjYjIwNTY2MzM4MGNkNCIsImV4cCI6MTY5MjI2OTM0NCwiaWF0IjoxNjg3MDg1MzQ0fQ.vmWyAAhOQ4qGGHLS-P5Jhp-kChRGeDmlcaWSvZ9i874";
-  // const [imageUrl, setImageUrl] = useState("");
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
 
-  useEffect(() => {
-    // if (image) {
-    //   setImagePath("https://api.mandarin.weniv.co.kr/" + image);
-    // }
-    // console.log("Image URL:", imageURL);
-  }, [post, image, previewImage]);
-
-  const handleFile = async (event) => {
-    const file = event.target.files[0];
-    console.log(file);
-    // Set Preview Image
-    const previewImageUrl = URL.createObjectURL(file);
-    setPreviewImage(previewImageUrl);
-    // console.log(previewImageUrl);
-    // Upload Image to Sever
-    const formData = new FormData();
-    formData.append("image", file);
-    try {
-      const response = await fetch("https://api.mandarin.weniv.co.kr/image/uploadfile", {
-        method: "POST",
-        body: formData,
-      });
-      const data = await response.json();
-      console.log(data);
-      setImage(data);
-      // setPreviewImage(data);
-    } catch (error) {
-      console.error(error);
-    }
+  const addComment = (newComment) => {
+    setComments((prevComments) => [...prevComments, newComment]);
   };
 
-  const handleUpload = async () => {
-    if (!post || !image) {
-      console.log("내용 또는 이미지를 입력해주세요.");
-      return;
-    }
-    const body = {
-      post: {
-        content: post,
-        image: "https://api.mandarin.weniv.co.kr/" + image.filename,
-      },
-    };
-    console.log(body);
-    console.log(token);
-    try {
-      const response = await fetch("https://api.mandarin.weniv.co.kr/post", {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {
-          "Content-type": "application/json",
-          Authorization: `Bearer ${token}`,
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleCommentSubmit = (event) => {
+    event.preventDefault();
+    if (comment.trim() !== "") {
+      const postId = "8989@test.com"; // 댓글을 달 게시물의 ID
+
+      const url = `https://api.mandarin.weniv.co.kr/product/post/${postId}/comments`;
+      const headers = {
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGQ2ZWYzYjJjYjIwNTY2MzM4MGNkNCIsImV4cCI6MTY5MjM2NDA5MywiaWF0IjoxNjg3MTgwMDkzfQ.vuyXTUmBLvvHsyizMnm1Wh7tT9vbbothySPTUZqZ5ho",
+        "Content-Type": "application/json",
+      };
+
+      const data = {
+        comment: {
+          content: comment,
         },
-      });
-      const data = await response.json();
-      console.log(data);
-      // 요청이 잘 되었을 경우: 프로필 페이지로 이동!!, 응답 데이터를 사용해야 하는지!
-      // setPost(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      };
 
-  const handleContentChange = (event) => {
-    setPost(event.target.value);
+      fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data),
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log("댓글 전송 완료!");
+            setComment("");
+            addComment(comment);
+          } else {
+            throw new Error("댓글 전송 실패");
+          }
+        })
+        .catch((error) => {
+          console.error("댓글 전송 실패:", error);
+        });
+    }
   };
 
   return (
     <>
-      <Header handlePostUpload={handleUpload} />
       <Upload>
         <h2 className="a11y-hidden">게시글 작성</h2>
 
         <div>
-          <Form>
-            <label htmlFor="txt-sync" className="a11y-hidden">
-              게시글 입력창입니다.
-            </label>
-            <Textarea
-              id="txt-sync"
-              // cols="40"
-              // rows="10"
-              maxLength="140"
-              placeholder="게시글 입력하기..."
-              className="upload-txt"
-              value={post}
-              onChange={handleContentChange}
-            ></Textarea>
+          <Form onSubmit={handleCommentSubmit}>
             <Label htmlFor="file-sync" className="file-sync"></Label>
-            <UploadInput type="file" id="file-sync" accept=".png, .jpg, .jpeg" multiple hidden onChange={handleFile} />
+            <UploadInput type="file" id="file-sync" accept=".png, .jpg, .jpeg" multiple hidden />
+            <Img src={profileImg} alt="profileImg" />
+            <input className="instaPost_input" type="text" placeholder="댓글 달기..." value={comment} onChange={handleCommentChange} />
+            <button type="submit">게시</button>
+            <div>
+              {comments.map((comment, index) => (
+                <p key={index}>{comment}</p>
+              ))}
+            </div>
           </Form>
-          <div className="img-container">
-            {/* {previewImage && (
-            <img
-              src={"https://api.mandarin.weniv.co.kr/" + previewImage.filename}
-              alt="Preview"
-            />
-          )} */}
-            {image && <img src={"https://api.mandarin.weniv.co.kr/" + image.filename} alt="Uploaded" />}
-          </div>
         </div>
       </Upload>
     </>
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import StartSplash from './../pages/Splash/StartSplash';
-import Login from '../pages/Login/Login';
-import SignUp from '../pages/SignUp/SignUp'
-import HomeFeed from '../pages/HomeFeed/HomeFeed';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import StartSplash from "./../pages/Splash/StartSplash";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import HomeFeed from "../pages/HomeFeed/HomeFeed";
 
 function Router() {
-    return (
-        <Routes>
-          <Route path="/" element={<StartSplash />}></Route>
-          <Route path="/Login" element={<Login />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/homefeed" element={<HomeFeed />}></Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<StartSplash />}></Route>
+      <Route path="/Login" element={<Login />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/homefeed" element={<HomeFeed />}></Route>
+    </Routes>
+  );
 }
 
 export default Router;

29b17c1ce52c7bb72377e8c34a3866dbf1230e39
yonainthefish <yonaisgood@gmail.com>
2023-06-20 19:25:08 +0900

    feat: handleForm 이벤트 추가하여 페이지이동 되게 변경

--- src/pages/SignUp/SignUpEmail/SignUpEmail.jsx
+++ src/pages/SignUp/SignUpEmail/SignUpEmail.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { SignUpContainer, Form, Input, H1, Label } from "./SignUpStyle";
-import { GreenBigButton, UnactiveBigButton } from "../../components/Button/Button";
+import { SignUpContainer, Form, Input, H1, Label } from "./SignUpEmailStyle";
+import { GreenBigButton, UnactiveBigButton } from "../../../components/Button/Button";
 
-export default function SignUp() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+export default function SignUpEmail({ 
+  setPage,
+  email,
+  setEmail,
+  password,
+  setPassword,}) {
   const [emailValid, setEmailValid] = useState(true);
   const [passwordValid, setPasswordValid] = useState(true);
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
+  //이메일 주소 기입
   const handleEmailInput = (event) => {
     setEmail(event.target.value);
   };
+
 
   // 비밀번호 유효성 검사
   const handlePasswordInput = (event) => {
@@ -52,24 +56,32 @@ export default function SignUp() {
     Msg === "사용 가능한 이메일 입니다." ? setEmailValid(true) : setEmailValid(false);
   };
 
+  // 이메일, 비밀번호가 통과되어 유효할 시 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (email && password && emailValid && passwordValid) {
     }
   };
 
+  // 기존페이지에서 넘어가도록 지정
+  const handleForm = (e) => {
+    e.preventDefault(); 
+    setPage('SignUpProfile');
+  };
+
   return (
     <>
       <SignUpContainer>
         <H1>이메일로 회원가입</H1>
-        <Form onSubmit={handleSubmit}>
+        <Form 
+         onSubmit={handleSubmit}>
           <Label htmlFor='user-email'>이메일</Label>
           <Input id={"user-email"} type={"email"} label={"이메일"} placeholder={"이메일 주소를 입력해 주세요."} value={email} valid={emailValid} alertMsg={setEmailError} onChange={handleEmailInput} onBlur={handleEmailValid} />
           {emailError && <p style={{ marginBottom: "2rem", marginTop: "-2.4rem", fontSize: "1.2rem", color: "var(--font-red-color)" }}>{emailError}</p>}
           <Label htmlFor='user-password'>비밀번호</Label>
           <Input id={"user-password"} type={"password"} label={"비밀번호"} placeholder={"비밀번호를 설정해 주세요."} value={password} valid={passwordValid} alertMsg={setPasswordError} onChange={handlePasswordInput} />
           {passwordError && <p style={{ marginBottom: "3rem", marginTop: "-2.4rem", fontSize: "1.2rem", color: "var(--font-red-color)" }}>{passwordError}</p>}
-          <Link to={"/ProfileSetting"} state={{ test: 1 }}>{email && password ? <GreenBigButton type='submit' contents={"다음"} /> : <UnactiveBigButton type='submit' contents={"다음"} />} </Link>
+          {email && password ? <GreenBigButton onClick={handleForm} type='submit' contents={"다음"} /> : <UnactiveBigButton type='submit' contents={"다음"} />} 
         </Form>
       </SignUpContainer>
     </>

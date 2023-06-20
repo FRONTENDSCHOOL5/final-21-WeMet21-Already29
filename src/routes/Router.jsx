f668c83c00feec43bcdb2a9dcfdc4ee0e0dadaf4
beomkwan.choi <3031011@naver.com>
2023-06-20 21:56:59 +0900

    feat: 공통 바텀시트 모달 컴포넌트 개발 완료

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -9,28 +9,41 @@ import ProductList from "../pages/ProductList/ProductList";
 import Profile from "../pages/Profile/Profile";
 import ProductDetail from "../pages/ProductDetail/ProductDetail";
 import ModalContext from "../contexts/ModalContext/ModalContext";
+import BottomSheetContext from "../contexts/ModalContext/BottomSheetContext";
+import BottomSheet from "../components/Modal/BottomSheet/BottomSheet";
 
 function Router() {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [isBottomSheetOpen, setBottomSheetOpen] = useState(false);
 
   return (
-    <ModalContext.Provider value={{ isModalOpen, setModalOpen }}>
-      <Routes>
-        <Route path="/" element={<StartSplash />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
-        {/* <Route path="/signup" element={<SignUp />}></Route> */}
-        <Route path="/homefeed" element={<HomeFeed />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/homefeed" element={<HomeFeed />}></Route>
-        <Route path="/uploadProduct" element={<UploadProduct />}></Route>
-        <Route path="/productlist/:id" element={<ProductList />}></Route>
-        <Route path="/profile/:id" element={<Profile />}></Route>
-        <Route path="/product/*" element={<Outlet />}>
-          <Route path="detail/:id" element={<ProductDetail />}></Route>
-          <Route path="modify/:id" element={<UploadProduct />}></Route>
-        </Route>
-      </Routes>
-    </ModalContext.Provider>
+    <BottomSheetContext.Provider value={{ isBottomSheetOpen, setBottomSheetOpen }}>
+      <ModalContext.Provider value={{ isModalOpen, setModalOpen }}>
+        <Routes>
+          <Route path="/Login" element={<Login />}></Route>
+          <Route
+            path="/"
+            element={
+              <BottomSheet>
+                <button onClick={() => console.log("hello")}>버튼1</button>
+                <button onClick={() => console.log("hello")}>버튼2</button>
+              </BottomSheet>
+            }
+          ></Route>
+          {/* <Route path="/signup" element={<SignUp />}></Route> */}
+          <Route path="/homefeed" element={<HomeFeed />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/homefeed" element={<HomeFeed />}></Route>
+          <Route path="/uploadProduct" element={<UploadProduct />}></Route>
+          <Route path="/productlist/:id" element={<ProductList />}></Route>
+          <Route path="/profile/:id" element={<Profile />}></Route>
+          <Route path="/product/*" element={<Outlet />}>
+            <Route path="detail/:id" element={<ProductDetail />}></Route>
+            <Route path="modify/:id" element={<UploadProduct />}></Route>
+          </Route>
+        </Routes>
+      </ModalContext.Provider>
+    </BottomSheetContext.Provider>
   );
 }
 

ce28a0a0adc6871b1937c302b032c83063965682
beomkwan.choi <126222927+KwanBeom@users.noreply.github.com>
2023-06-20 22:04:19 +0900

    docs: 라우터 초기 세팅으로 변경

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,50 +1,18 @@
-import React, { useState } from "react";
-import { Outlet, Route, Routes } from "react-router-dom";
-import StartSplash from "./../pages/Splash/StartSplash";
-import Login from "../pages/Login/Login";
-// import SignUp from "../pages/SignUp/SignUp";
-import HomeFeed from "../pages/HomeFeed/HomeFeed";
-import UploadProduct from "../pages/UploadProduct/UploadProduct";
-import ProductList from "../pages/ProductList/ProductList";
-import Profile from "../pages/Profile/Profile";
-import ProductDetail from "../pages/ProductDetail/ProductDetail";
-import ModalContext from "../contexts/ModalContext/ModalContext";
-import BottomSheetContext from "../contexts/ModalContext/BottomSheetContext";
-import BottomSheet from "../components/Modal/BottomSheet/BottomSheet";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import StartSplash from './../pages/Splash/StartSplash';
+import Login from '../pages/Login/Login';
+import Home from "../pages/HomeFeed/HomeFeed";
 
 function Router() {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [isBottomSheetOpen, setBottomSheetOpen] = useState(false);
-
-  return (
-    <BottomSheetContext.Provider value={{ isBottomSheetOpen, setBottomSheetOpen }}>
-      <ModalContext.Provider value={{ isModalOpen, setModalOpen }}>
-        <Routes>
-          <Route path="/Login" element={<Login />}></Route>
-          <Route
-            path="/"
-            element={
-              <BottomSheet>
-                <button onClick={() => console.log("hello")}>버튼1</button>
-                <button onClick={() => console.log("hello")}>버튼2</button>
-              </BottomSheet>
-            }
-          ></Route>
-          {/* <Route path="/signup" element={<SignUp />}></Route> */}
-          <Route path="/homefeed" element={<HomeFeed />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/homefeed" element={<HomeFeed />}></Route>
-          <Route path="/uploadProduct" element={<UploadProduct />}></Route>
-          <Route path="/productlist/:id" element={<ProductList />}></Route>
-          <Route path="/profile/:id" element={<Profile />}></Route>
-          <Route path="/product/*" element={<Outlet />}>
-            <Route path="detail/:id" element={<ProductDetail />}></Route>
-            <Route path="modify/:id" element={<UploadProduct />}></Route>
-          </Route>
-        </Routes>
-      </ModalContext.Provider>
-    </BottomSheetContext.Provider>
-  );
+    return (
+      <Routes>
+          <Route path="/" element={<StartSplash />}></Route>
+          <Route path="/Login" element={<Login />}></Route>   
+        <Route path="/home" element={<Home />} />     
+      </Routes>
+      
+    );
 }
 
 export default Router;

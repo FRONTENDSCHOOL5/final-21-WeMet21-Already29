32b7cb6e8b3d0da572e161fa4a6b09a346b1275b
yonainthefish <yonaisgood@gmail.com>
2023-06-21 02:23:48 +0900

    style: 게시글 댓글에 따라 스크롤 생성 추가

--- src/pages/PostDetail/PostDetailStyle.jsx
+++ src/pages/PostDetail/PostDetailStyle.jsx
@@ -1,17 +1,104 @@
 import styled from "styled-components";
 
 const Upload = styled.section`
-  border: 1px solid red;
-  padding: 2rem 1.6rem;
+  border: 5px solid red;
   display: flex;
-
   align-items: start;
   gap: 1.2rem;
   position: relative;
-  height: calc(100vh - 8.8rem);
 `;
 
-const Form = styled.form``;
+const CommentInput = styled.div`
+  border-top: 1px solid var(--line-gray-color);
+  padding: 1.2rem 1.6rem;
+  display: flex;
+  justify-content: space-between;
+  align-items:center;
+
+  .instaPost_input {
+    width: 26rem;
+    margin: 0 1.8rem;
+    border:0px;
+    font-size:1.4rem;
+    font-weight:400;
+
+  
+    
+    ::placeholder{
+      color: #c4c4c4;
+      font-size: 1.4rem;
+      font-weight: 400;
+    }
+
+      &:focus {
+      outline: 0;
+      nofocus:focus {
+        outline: none;
+      }
+    }    
+  }
+
+  .uploadBtn{
+    border: 0px;
+    background-color:transparent;
+    width: 5rem;
+    font-size:1.4rem;
+    color: #c4c4c4;
+    font-weight:500;
+  }
+
+  .uploadBtn.active{
+    color: var(--main-color);
+    font-weight: 700;
+  }
+
+`;
+
+const Div = styled.div`
+  width:100%;  
+`;
+
+const Form = styled.form`
+  width:100%;
+`;
+
+const CommnetDiv = styled.div`
+  border:1px solid red;
+  padding: 2rem 1.6rem;
+  overflow-y: auto;
+  max-height: 23.6rem;
+
+  &::-webkit-scrollbar {
+    width:0.9rem;
+  };
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #c4c4c4; /* 스크롤 막대 색상 */
+    border-radius: 3px;
+  };
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  };
+`;
+
+const SmallDiv = styled.div`
+  display:flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  gap:1.2rem;
+  margin-bottom:1.6rem;
+
+  .time{
+    font-size: 1rem;
+    color: var(--gray-color);
+  }
+  .time::before{
+    content:"·";
+    margin-right:0.5rem;
+  }
+`;
 
 const Textarea = styled.textarea`
   border: 0px;
@@ -36,7 +123,10 @@ const Textarea = styled.textarea`
 
 const UploadInput = styled.input``;
 
-const Img = styled.img``;
+const Img = styled.img`
+  width: 3.6rem;
+  height: 3.6rem;  
+`;
 
 const Label = styled.label`
   cursor: pointer;
@@ -45,4 +135,4 @@ const Label = styled.label`
   bottom: 1.6rem;
 `;
 
-export { Upload, Form, UploadInput, Img, Label, Textarea };
+export { Upload, CommentInput, Div, Form, UploadInput, Img, Label, Textarea, CommnetDiv, SmallDiv };

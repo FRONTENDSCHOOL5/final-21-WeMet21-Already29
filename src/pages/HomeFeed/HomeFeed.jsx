8c359a5fffd724a1a6de20facdd5b59a3a14e659
SangHun Han <93968241+Skyllerrr@users.noreply.github.com>
2023-06-20 22:18:54 +0900

    Update HomeFeed.jsx

--- src/pages/HomeFeed/HomeFeed.jsx
+++ src/pages/HomeFeed/HomeFeed.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import StartSplash from './../pages/Splash/StartSplash';
-import Login from '../pages/Login/Login';
-import Home from "../pages/HomeFeed/HomeFeed";
-import SearchPage from "../pages/Search/SearchPage";
+import React from "react";
+import { Container } from "./HomeFeedStyle";
+import Navigation from "../../components/Footer/FooterMenu/FooterMenu";
+import PostHome from "./PostHomeFeed";
+import EmptyHome from "./EmptyHomeFeed";
 
-function Router() {
-    return (
-      <Routes>
-          <Route path="/" element={<StartSplash />}></Route>
-          <Route path="/Login" element={<Login />}></Route>   
-        <Route path="/home" element={<Home />} />     
-        <Route path="/search" element={<SearchPage />}></Route>
-      </Routes>
-      
-    );
+export default function Home() {
+  return (
+    <Container>
+      {/* <EmptyHome /> */}
+      <PostHome />
+      <Navigation />
+    </Container>
+  );
 }
-
-export default Router;

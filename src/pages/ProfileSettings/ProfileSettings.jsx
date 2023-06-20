bf30b1db53984224ef45a7c64fd6ac5358fa0f2c
yonainthefish <yonaisgood@gmail.com>
2023-06-20 09:36:55 +0900

    feat: 프로필이미지 변경 기능 추가

--- src/pages/ProfileSettings/ProfileSettings.jsx
+++ src/pages/ProfileSettings/ProfileSettings.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
-import { ProfileSection, ProfileTile, ProfileInfo, EditWrap, UploadButton, EditForm, Label, Input } from "./ProfileSettingsStyle";
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { ProfileSection, ProfileTile, ProfileInfo, ImgUploadBtn, UploadInput, EditForm, Label, Input, Img, ImgIcon } from "./ProfileSettingsStyle";
 import { GreenBigButton, UnactiveBigButton } from "../../components/Button/Button";
-import uploadImg from "../../assets/images/uploadImg.png";
-export default function ProfileSettings() {
-  const [productImage, setproductImage] = useState("");
-  const [productImageUrl, setproductImageUrl] = useState("https://api.mandarin.weniv.co.kr/1686629045637.png");
+import uploadFile from "../../assets/images/basicProfileImg.png";
+import uploadIcon from "../../assets/images/uploadFile.png";
 
+export default function ProfileSettings() {
+  const uploadInputRef = useRef(null);
+  const [post, setPost] = useState("");
+  const [imageUrl, setImageUrl] = useState(""); // 이미지 URL 상태 추가
+
+  const [image, setImage] = useState(null); // 추후에 다중 이미지로 작업할 때는 image를 배열로!
   const [name, setName] = useState("");
   const [id, setId] = useState("");
   const [introduce, setIntroduce] = useState("");
@@ -14,37 +19,37 @@ export default function ProfileSettings() {
   const [nameError, setNameError] = useState("");
   const [idError, setIdError] = useState("");
 
+  const handleImgClick = () => {
+    uploadInputRef.current.click();
+  };
+
+  useEffect(() => {
+  }, [post, image]);
+
   //프로필 사진 업로드
-  const handleImgInput = async (e) => {
-    if (e.target.files.length === 0) {
-      return;
-    }
+  const handleFile = async (event) => {
+    const file = event.target.files[0];
+    
+    const profile = document.querySelector("#profile"); //1. 접근 (이미지 요소가 변수 안에 담김)
+    const url = "https://api.mandarin.weniv.co.kr/" // 2. url요청해오기 
+    
     const formData = new FormData();
-    const userImg = e.target.files[0];
-    if (userImg.size > 1000000) {
-      alert("1MB 미만의 이미지 파일만 업로드 가능합니다.");
-      return;
+    formData.append("image", file);
+    try {
+      const response = await fetch("https://api.mandarin.weniv.co.kr/image/uploadfile", {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+      const path = url + data.filename // 3. 이미지 주소
+      profile.src = path; // 4. path에 저장해 놓은 값을 불러옴 (실질적 이미지 보여주는 작업)
+
+      setImageUrl(path); // 이미지 URL 업데이트
+      setImage(data);
+      // console.log(data);
+    } catch (error) {
+      console.error(error);
     }
-
-    const fileNamesplitArray = userImg.name.split(".");
-    const fileExtension = fileNamesplitArray[fileNamesplitArray.length - 1];
-    const fileExtensionValues = ["jpg", "gif", "png", "jpeg", "bmp", "tif", "heic"];
-
-    if (!fileExtensionValues.includes(fileExtension)) {
-      alert("이미지 파일만 업로드 가능합니다.");
-      return;
-    }
-
-    setproductImage(e.target.value);
-    formData.append("image", userImg);
-
-    const res = await fetch("https://api.mandarin.weniv.co.kr/image/uploadfile", {
-      method: "POST",
-      body: formData,
-    });
-    const json = await res.json();
-    console.log(json);
-    setproductImageUrl(`https://api.mandarin.weniv.co.kr/${json.filename}`);
   };
 
   // 사용자 이름 유효성 검사
@@ -83,6 +88,7 @@ export default function ProfileSettings() {
       setIdError("");
       setIdValid(true);
     } else {
+      setId(value); // 입력된 값을 그대로 유지 (첫번째 글자 지워지지 않는 오류 해결)
       setIdError("영문, 숫자, 특수문자(.),(_)만 사용 가능합니다");
       setIdValid(false);
     }
@@ -105,11 +111,14 @@ export default function ProfileSettings() {
         <h2 className='a11y-hidden'>프로필 설정</h2>
         <ProfileTile>프로필 설정</ProfileTile>
         <ProfileInfo>나중에 언제든지 변경할 수 있습니다.</ProfileInfo>
-        <EditWrap>
-          <UploadButton type='file' onChange={handleImgInput}>
-            <img style={{ width: "11rem" }} src={uploadImg} alt='프로필 이미지 업로드 버튼입니다.' />
-          </UploadButton>
-        </EditWrap>
+        <Label htmlFor="file-sync" className="file-sync"  onClick={handleImgClick}>
+          <ImgUploadBtn>
+            <Img src={uploadFile} alt="uploadFile" />  
+            <ImgIcon src={uploadIcon} alt="업로드아이콘" />   
+          </ImgUploadBtn>
+        </Label>
+        <UploadInput ref={uploadInputRef} id='profile' type="file"  accept=".png, .jpg, .jpeg" multiple hidden onChange={handleFile} />         
+        {imageUrl && <img src={imageUrl} alt="프로필 사진" />}
         <EditForm onSubmit={handleSubmit}>
           <Label htmlFor='user-name'>사용자이름</Label>
           <Input id={"user-name"} type={"text"} label={"사용자이름"} placeholder={"2~10자 이내여야 합니다."} value={name} alertMsg={setNameError} onChange={handleNameInput} onBlur={handleNameInput} required />
@@ -119,8 +128,9 @@ export default function ProfileSettings() {
           {idError && <p style={{ marginBottom: "2rem", marginTop: "-2.4rem", fontSize: "1.2rem", color: "var(--font-red-color)" }}>{idError}</p>}
           <Label htmlFor='user-introduce'>소개</Label>
           <Input id={"user-introduce"} type={"text"} label={"소개"} placeholder={"좋아하는 브랜드와 룩을 알려주세요."} value={introduce} onChange={handleIntroduceInput} required />
-
-          {name && id && introduce ? <GreenBigButton type='submit' contents={"입9팔9 즐기러 가기"} /> : <UnactiveBigButton type='submit' contents={"입9팔9 즐기러 가기"} />}
+          <Link to ="/">
+            {name && id && introduce ? <GreenBigButton type='submit' contents={"입9팔9 즐기러 가기"} /> : <UnactiveBigButton type='submit' contents={"입9팔9 즐기러 가기"} />}
+          </Link>
         </EditForm>
       </ProfileSection>
     </>

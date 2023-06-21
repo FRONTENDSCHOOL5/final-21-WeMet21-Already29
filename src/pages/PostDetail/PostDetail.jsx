b51a7b4f110a06dcd2c714987b6314ba7277f664
kyeongboo-coder <kyeong0914@gmail.com>
2023-06-21 15:46:29 +0900

    feat : 댓글 구현 기능 완료

--- src/pages/PostDetail/PostDetail.jsx
+++ src/pages/PostDetail/PostDetail.jsx
@@ -1,41 +1,59 @@
 import React, { useEffect, useState } from "react";
 // import { useParams } from "react-router";
 import profileImg from "../../assets/images/profileImg.png";
-import { Upload, CommentInput, Div, Form, UploadInput, Label, Img, CommnetDiv, SmallDiv } from "./PostDetailStyle";
+import IconMoreVertical from "../../assets/images/IconMoreVertical.png";
+import AlertModal from "../../components/Modal/AlertModal/AlertModal";
+import { Upload, Form, UploadInput, Label, Img } from "./PostDetailStyle";
 
 export default function PostDetail() {
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
-  const [isCommentNotEmpty, setIsCommentNotEmpty] = useState(false);
 
-  // const [post, setPost] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [targetCommentId, setTargetCommentId] = useState("");
 
   const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGQ2ZWYzYjJjYjIwNTY2MzM4MGNkNCIsImV4cCI6MTY5MjM2NDA5MywiaWF0IjoxNjg3MTgwMDkzfQ.vuyXTUmBLvvHsyizMnm1Wh7tT9vbbothySPTUZqZ5ho";
 
   // 임시로 /post/feed로 가져온 게시글 중 하나의 id로 설정
   const postId = "648c2a98b2cb20566336745f";
 
+  // 나의 username
+  const username = "8989";
+
   const addComment = (newComment) => {
     console.log(newComment);
 
-    setComments((prevComments) => [...prevComments, newComment]);
+    setComments((prevComments) => [newComment, ...prevComments]);
   };
 
   const calculateElapsedTime = (timestamp) => {
+    const createdTime = new Date(timestamp).getTime();
     const currentTime = new Date().getTime();
-    const elapsedMilliseconds = currentTime - timestamp;
-    const elapsedMinutes = Math.floor(elapsedMilliseconds / (1000 * 60));
-    return elapsedMinutes > 0 ? `${elapsedMinutes}분 전` : `방금 전`;
+    const elapsedMilliseconds = currentTime - createdTime; // 여기서 결과가 NaN
+    const elapsedSeconds = Math.floor(elapsedMilliseconds / 1000);
+    const elapsedMinutes = Math.floor(elapsedSeconds / 60);
+    const elapsedHours = Math.floor(elapsedMinutes / 60);
+    const elapsedDays = Math.floor(elapsedHours / 24);
+
+    if (elapsedDays > 0) {
+      return `${elapsedDays}일 전`;
+    } else if (elapsedHours > 0) {
+      return `${elapsedHours}시간 전`;
+    } else if (elapsedMinutes > 0) {
+      return `${elapsedMinutes}분 전`;
+    } else {
+      return "방금 전";
+    }
   };
 
   const handleCommentChange = (event) => {
     setComment(event.target.value);
   };
 
-  // 코멘트 작성시 게시버튼 변화
-  useEffect(() => {
-    setIsCommentNotEmpty(comment.trim() !== "");
-  }, [comment]);
+  const handleOpenModal = (commentId) => {
+    setTargetCommentId(commentId);
+    setIsModalOpen(true); // 모달 오픈
+  };
 
   // 댓글 리스트
   const getCommentList = () => {
@@ -50,7 +68,7 @@ export default function PostDetail() {
       .then((result) => {
         console.log(result);
 
-        // setComments()
+        setComments(result.comments);
       })
       .catch((error) => {
         console.error("댓글 불러오기 실패:", error);
@@ -65,7 +83,7 @@ export default function PostDetail() {
 
       const url = `https://api.mandarin.weniv.co.kr/post/${postId}/comments`;
       const headers = {
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGQ2ZWYzYjJjYjIwNTY2MzM4MGNkNCIsImV4cCI6MTY5MjM2NDA5MywiaWF0IjoxNjg3MTgwMDkzfQ.vuyXTUmBLvvHsyizMnm1Wh7tT9vbbothySPTUZqZ5ho",
+        Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       };
 
@@ -85,7 +103,7 @@ export default function PostDetail() {
           if (result.status === 404) throw new Error("댓글 전송 실패");
           else {
             setComment("");
-            // addComment(comment); 이전 코드
+
             addComment(result.comment);
           }
         })
@@ -95,7 +113,35 @@ export default function PostDetail() {
     }
   };
 
- 
+  useEffect(() => {
+    getCommentList();
+  }, []);
+
+  // 댓글 삭제
+  const deleteComment = () => {
+    fetch(`https://api.mandarin.weniv.co.kr/post/${postId}/comments/${targetCommentId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+
+        if (result.status === "200") {
+          setIsModalOpen(false); // 모달 닫기
+          getCommentList(); // 다시 댓글 리스트 가져오기 -> 재렌더링
+        } else {
+          throw new Error("댓글 삭제 실패");
+        }
+      })
+      .catch((error) => {
+        console.error("댓글 삭제 실패:", error);
+      });
+  };
+
   useEffect(() => {
     getCommentList();
   }, []);
@@ -105,31 +151,34 @@ export default function PostDetail() {
       <Upload>
         <h2 className="a11y-hidden">게시글 작성</h2>
 
-        <Div>
+        <div>
           <Form onSubmit={handleCommentSubmit}>
-            
-            <CommnetDiv>
+            <Label htmlFor="file-sync" className="file-sync"></Label>
+            <UploadInput type="file" id="file-sync" accept=".png, .jpg, .jpeg" multiple hidden />
+            <Img src={profileImg} alt="profileImg" />
+            <input className="instaPost_input" type="text" placeholder="댓글 달기..." value={comment} onChange={handleCommentChange} />
+            <button type="submit">게시</button>
+            <div>
               {comments.length > 0 &&
                 comments.map((comment, index) => (
-              <SmallDiv key={index}>
-                <div>
-                <Img src={comment.author.image} alt="profileImg" />
-                </div>
-                <p style={{fontSize:"1.4rem", fontWeight:"500"}}>{comment.author.username}</p>
-                <p className='time'>{calculateElapsedTime(comment.createdAt)}</p>
-                <p style={{fontSize:"1.4rem", color:"#333"}}>{comment.content}</p>
-              </SmallDiv>
+                  <div key={index}>
+                    <Img src={comment.author.image} alt="profileImg" />
+                    <p>{comment.author.username}</p>
+                    <p>{comment.content}</p>
+                    <p>{calculateElapsedTime(comment.createdAt)}</p>
+                    {comment.author.username === username && (
+                      <div>
+                        <button type="button" className="more" onClick={() => handleOpenModal(comment.id)}>
+                          <img src={IconMoreVertical} alt="" />
+                        </button>
+                      </div>
+                    )}
+                  </div>
                 ))}
-            </CommnetDiv>
-            <CommentInput>  
-              <Label htmlFor="file-sync" className="file-sync"></Label>
-              <UploadInput type="file" id="file-sync" accept=".png, .jpg, .jpeg" multiple hidden />
-              <Img src={profileImg} alt="profileImg" />
-              <input className="instaPost_input" type="text" placeholder="댓글 입력하기..." value={comment} onChange={handleCommentChange} />
-              <button className={isCommentNotEmpty ? "uploadBtn active" : "uploadBtn"} type="submit">게시</button>
-            </CommentInput>
+            </div>
+            {isModalOpen && <AlertModal onSubmit={deleteComment} onCancel={() => setIsModalOpen(false)} submitText="삭제" children="댓글을 삭제할까요?" />}
           </Form>
-        </Div>
+        </div>
       </Upload>
     </>
   );

d8df93ca9328b19ce8b70764762fb46a4bece840
yonainthefish <yonaisgood@gmail.com>
2023-06-21 17:04:02 +0900

    style: 스타일컴포넌트 코드 정리

--- src/pages/PostDetail/PostDetailStyle.jsx
+++ src/pages/PostDetail/PostDetailStyle.jsx
@@ -1,75 +1,23 @@
 import styled from "styled-components";
 
 const Upload = styled.section`
-  border: 5px solid red;
   display: flex;
   align-items: start;
   gap: 1.2rem;
   position: relative;
 `;
 
-const CommentInput = styled.div`
-  border-top: 1px solid var(--line-gray-color);
-  padding: 1.2rem 1.6rem;
-  display: flex;
-  justify-content: space-between;
-  align-items:center;
-
-  .instaPost_input {
-    width: 26rem;
-    margin: 0 1.8rem;
-    border:0px;
-    font-size:1.4rem;
-    font-weight:400;
-
-  
-    
-    ::placeholder{
-      color: #c4c4c4;
-      font-size: 1.4rem;
-      font-weight: 400;
-    }
-
-      &:focus {
-      outline: 0;
-      nofocus:focus {
-        outline: none;
-      }
-    }    
-  }
-
-  .uploadBtn{
-    border: 0px;
-    background-color:transparent;
-    width: 5rem;
-    font-size:1.4rem;
-    color: #c4c4c4;
-    font-weight:500;
-  }
-
-  .uploadBtn.active{
-    color: var(--main-color);
-    font-weight: 700;
-  }
-
-`;
-
-const Div = styled.div`
-  width:100%;  
-`;
-
 const Form = styled.form`
-  width:100%;
+  width: 100%;
 `;
 
 const CommnetDiv = styled.div`
-  border:1px solid red;
   padding: 2rem 1.6rem;
   overflow-y: auto;
-  height:23.6rem;
+  height: 23.6rem;
 
   &::-webkit-scrollbar {
-    width:0.9rem;
+    width: 0.9rem;
   };
 
   &::-webkit-scrollbar-thumb {
@@ -83,20 +31,21 @@ const CommnetDiv = styled.div`
 `;
 
 const SmallDiv = styled.div`
-  display:flex;
-  flex-direction:column;
+  display: flex;
+  flex-direction: column;
   justify-content: start;
   align-items: start;
-  gap:1.2rem;
-  margin-bottom:1.6rem;
+  gap: 1.2rem;
+  margin-bottom: 1.6rem;
+  position: relative;
 
   .time{
     font-size: 1rem;
     color: var(--gray-color);
   }
   .time::before{
-    content:"·";
-    margin-right:0.5rem;
+    content: "·";
+    margin-right: 0.5rem;
   }
 `;
 
@@ -105,35 +54,59 @@ const Namediv = styled.div`
   flex-direction: row; 
   align-items: center;
   gap: 1.2rem;
-  /* border:1px solid green; */
 `;
 
-const Textarea = styled.textarea`
-  border: 0px;
-  resize: none;
-  width: 100%;
-  height: 100%;
-  margin-top: 1.2rem;
+const VerticalBtn = styled.button`
+  border: 0;
+  background-color : transparent;
+  position: absolute;
+  right: -1rem;
+  top: 0.8rem;
+  padding: 0px;
+  cursor: pointer;
+`;
 
-  &:focus {
-    outline: 0;
-    nofocus:focus {
-      outline: none;
-    }
-  }
+const CommentInput = styled.div`
+  border-top: 1px solid var(--line-gray-color);
+  padding: 1.2rem 1.6rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-  ::placeholder {
-    color: var(--gray-color);
-    font-weight: 400;
+  .instaPost_input {
+    width: 26rem;
+    margin: 0 1.8rem;
+    border: 0px;
     font-size: 1.4rem;
+    font-weight: 400;  
+    
+    ::placeholder{
+      color: #c4c4c4;
+      font-size: 1.4rem;
+      font-weight: 400;
+    }
+
+      &:focus {
+      outline: 0;
+      nofocus: focus {
+        outline: none;
+      }
+    }    
   }
-`;
 
-const UploadInput = styled.input``;
+  .uploadBtn{
+    border: 0px;
+    background-color: transparent;
+    width: 5rem;
+    font-size: 1.4rem;
+    color: #c4c4c4;
+    font-weight: 500;
+  }
 
-const Img = styled.img`
-  width: 3.6rem;
-  height: 3.6rem;  
+  .uploadBtn.active{
+    color: var(--main-color);
+    font-weight: 700;
+  }
 `;
 
 const Label = styled.label`
@@ -143,4 +116,10 @@ const Label = styled.label`
   bottom: 1.6rem;
 `;
 
-export { Upload, CommentInput, Div, Form, UploadInput, Img, Label, Textarea, CommnetDiv, SmallDiv, Namediv };
+const Img = styled.img`
+  width: 3.6rem;
+  height: 3.6rem;  
+`;
+
+
+export { Upload, Form, CommnetDiv, SmallDiv, Namediv, VerticalBtn, CommentInput, Label, Img  };

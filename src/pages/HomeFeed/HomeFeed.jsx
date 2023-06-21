b62ccb354a293c6ed9cf781c0366a49de5e06c21
hansanghun <rkdla666@naver.com>
2023-06-21 12:33:43 +0900

    fix : HomeFeed 헤더 추가

--- src/pages/HomeFeed/HomeFeed.jsx
+++ src/pages/HomeFeed/HomeFeed.jsx
@@ -3,12 +3,12 @@ import { Container } from "./HomeFeedStyle";
 import Navigation from "../../components/Footer/FooterMenu/FooterMenu";
 import PostHome from "./PostHomeFeed";
 import EmptyHome from "./EmptyHomeFeed";
-import Header from "../../components/HHeader/Header";
+import Header from '../../components/Header/Header';
 
 export default function Home() {
   return (
     <Container>
-      <Header type="home" />
+      <Header type="logo"/>
       {/* <EmptyHome /> */}
       <PostHome />
       <Navigation />

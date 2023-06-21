67e6860a6b36ab9f81e6d6b8845d4b2767915145
SangHun Han <93968241+Skyllerrr@users.noreply.github.com>
2023-06-21 18:04:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import GlobalStyle from "./style/GlobalStyle";
 import SignUp from "./pages/SignUp/SignUp"
-import { AuthContextProvider } from './contexts/SearchContext/Auth';
 
 function App() {
   return (
     <>
-      <AuthContextProvider>
         <GlobalStyle />
         <SignUp />
-      </AuthContextProvider>
     </>
   );
 };

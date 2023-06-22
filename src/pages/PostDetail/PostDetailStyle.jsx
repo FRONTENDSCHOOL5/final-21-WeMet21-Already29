2993a2affbe1fe7fe58ab882684b4559e3f1621f
beomkwan.choi <3031011@naver.com>
2023-06-23 00:50:39 +0900

    docs: 게시물 상세 페이지 스타일 수정

--- src/pages/PostDetail/PostDetailStyle.jsx
+++ src/pages/PostDetail/PostDetailStyle.jsx
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 
 const Upload = styled.section`
-  display: flex;
-  align-items: start;
-  gap: 1.2rem;
   position: relative;
 `;
 
@@ -12,29 +9,29 @@ const Form = styled.form`
 `;
 
 const Text = styled.div`
-  line-height: 2rem; 
-  font-size:1.4rem; 
-  color:#333333; 
+  line-height: 2rem;
+  font-size: 1.4rem;
+  color: #333333;
   margin-left: 4.8rem;
 `;
 
 const CommnetDiv = styled.div`
   padding: 2rem 1.6rem;
-  overflow-y: auto;
-  height: 23.6rem;
+  box-sizing: border-box;
+  margin-bottom: 40px;
 
   &::-webkit-scrollbar {
     width: 0.9rem;
-  };
+  }
 
   &::-webkit-scrollbar-thumb {
     background-color: #e2e2e2; /* 스크롤 막대 색상 */
     border-radius: 3px;
-  };
+  }
 
   &::-webkit-scrollbar-track {
     background-color: transparent;
-  };
+  }
 `;
 
 const SmallDiv = styled.div`
@@ -45,11 +42,11 @@ const SmallDiv = styled.div`
   margin-bottom: 1.6rem;
   position: relative;
 
-  .time{
+  .time {
     font-size: 1rem;
     color: var(--gray-color);
   }
-  .time::before{
+  .time::before {
     content: "·";
     margin-right: 0.5rem;
   }
@@ -57,14 +54,14 @@ const SmallDiv = styled.div`
 
 const Namediv = styled.div`
   display: flex;
-  flex-direction: row; 
+  flex-direction: row;
   align-items: center;
   gap: 1.2rem;
 `;
 
 const VerticalBtn = styled.button`
   border: 0;
-  background-color : transparent;
+  background-color: transparent;
   position: absolute;
   right: -1rem;
   top: 0.8rem;
@@ -76,31 +73,34 @@ const CommentInput = styled.div`
   border-top: 1px solid var(--line-gray-color);
   padding: 1.2rem 1.6rem;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  position: fixed;
+  bottom: 0;
+  width: 390px;
+  background-color: #fff;
+  box-sizing: border-box;
 
   .instaPost_input {
     width: 26rem;
     margin: 0 1.8rem;
     border: 0px;
     font-size: 1.4rem;
-    font-weight: 400;  
-    
-    ::placeholder{
+    font-weight: 400;
+
+    ::placeholder {
       color: #c4c4c4;
       font-size: 1.4rem;
       font-weight: 400;
     }
 
-      &:focus {
+    &:focus {
       outline: 0;
       nofocus: focus {
         outline: none;
       }
-    }    
+    }
   }
 
-  .uploadBtn{
+  .uploadBtn {
     border: 0px;
     background-color: transparent;
     width: 5rem;
@@ -109,7 +109,7 @@ const CommentInput = styled.div`
     font-weight: 500;
   }
 
-  .uploadBtn.active{
+  .uploadBtn.active {
     color: var(--main-color);
     font-weight: 700;
   }
@@ -124,8 +124,7 @@ const Label = styled.label`
 
 const Img = styled.img`
   width: 3.6rem;
-  height: 3.6rem;  
+  height: 3.6rem;
 `;
 
-
-export { Upload, Form, Text, CommnetDiv, SmallDiv, Namediv, VerticalBtn, CommentInput, Label, Img  };
+export { Upload, Form, Text, CommnetDiv, SmallDiv, Namediv, VerticalBtn, CommentInput, Label, Img };

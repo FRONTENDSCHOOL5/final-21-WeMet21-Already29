781d0fa636434181672d0e19cfd15f138b5083bb
hansanghun <rkdla666@naver.com>
2023-06-23 13:38:03 +0900

    feat: 업로드된 게시글이 있을 때 HomeFeed 화면

--- src/pages/HomeFeed/PostHomeFeed.jsx
+++ src/pages/HomeFeed/PostHomeFeed.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PostItem from "../../components/Post/PostItem/PostItem";
 import { List } from "./PostHomeFeedStyle";
+import axios from "axios";
 
-export default function PostHome() {
+export default function PostHome({myFeed, setMyFeed}) {
+  const getFeed = (page) => {
+    return axios.get(
+      `https://api.mandarin.weniv.co.kr/post/feed/?limit=10&skip=${page * 10}`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-type": "application/json",
+        },
+      },
+    )
+  }
+  
+useEffect(() => {
+  getFeed(1).then(res=> {
+    console.log(res)
+    setMyFeed(res.data.posts)})
+}, []);
+
   return (
     <List>
-      <PostItem />
+      <PostItem myFeed={myFeed} />
     </List>
   );
 }
+
+

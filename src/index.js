81dc4e58ab0fe2383837a15c9ee99912f3f1b8f5
beomkwan.choi <3031011@naver.com>
2023-06-24 21:59:06 +0900

    feat: lazy 로딩과 React Suspense 활용 로딩 구현

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Loading from "./components/Loading";
+const App = lazy(() => import("./App"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <Suspense fallback={<Loading />}>
+    <App />
+  </Suspense>
+);

d5b00c062e7ba646a62c53507219fd22a8fde5a7
hansanghun <rkdla666@naver.com>
2023-06-25 02:14:05 +0900

    feat: 팔로워 및 팔로잉 리스트 스타일링

---
+++ src/pages/FollowerList/FollowerListStyle.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const FollowList = styled.ul`
+  width: 358px;
+  padding: 60px 16px 24px;
+  background-color: #fff;
+  height: 100vh;
+`;
+
+export const FollowListItem = styled.li`
+  margin-bottom: 12px;
+`;
+

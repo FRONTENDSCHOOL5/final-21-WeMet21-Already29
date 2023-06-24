1391ee9c06636cf38df2c979ad71215cbe555403
한상헌 <hansanghun@hansangheon-ui-MacBookAir.local>
2023-06-24 17:13:22 +0900

    feat: 유저를 검색하는 Search 페이지 구현

--- src/pages/Search/SearchPage.jsx
+++ src/pages/Search/SearchPage.jsx
@@ -3,76 +3,85 @@ import StyledSearch from './SearchPageStyle'
 import Header from '../../components/Header/Header';
 import TabMenu from '../../components/Footer/FooterMenu/FooterMenu';
 import UserList from '../../components/UserList/UserList';
-//import useAuthContext from '../../hooks/useAuthContext';
+import { useNavigate } from "react-router-dom";
 
 function Search() {
-  const [searchList, setSearchList] = useState([]);
-  const [keyword, setKeyword] = useState('');
-  //const { auth } = useAuthContext();
-
-  useEffect(() => {
-    if (keyword) {
-      const GetUserInfo = async () => {
-        try {
-          const response = await fetch(
-            `https://api.mandarin.weniv.co.kr/user/searchuser/?keyword=${keyword}`,
-            {
-              method: 'GET',
-              headers: {
-                Authorization: `Bearer ${localStorage.getItem("token")}`,
-                'Content-type': 'application/json',
-              },
-            }
-          );
-          const result = await response.json();
-
-          setSearchList(result);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      GetUserInfo();
+    const navigate = useNavigate();
+    const [searchList, setSearchList] = useState([]);
+    const [keyword, setKeyword] = useState('');
+  
+    function handleClick(accountname) {
+      console.log(accountname);
+      navigate(`/profile/${accountname}`, {
+        state: {
+          accountname: accountname,
+        },
+      });
     }
-  }, [keyword]);
 
-  useEffect(() => {
-    const loading = setTimeout(() => {
-      if (keyword !== '' && !keyword.startsWith('')) {
-        Search();
+    useEffect(() => {
+      if (keyword) {
+        const GetUserInfo = async () => {
+          try {
+            const response = await fetch(
+              `https://api.mandarin.weniv.co.kr/user/searchuser/?keyword=${keyword}`,
+              {
+                method: 'GET',
+                headers: {
+                  Authorization: `Bearer ${localStorage.getItem("token")}`,
+                  'Content-type': 'application/json',
+                },
+              }
+            );
+            const result = await response.json();
+
+            setSearchList(result);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+        GetUserInfo();
       }
-    }, 100);
+    }, [keyword]);
 
-    return () => {
-      clearTimeout(loading);
-    };
-  }, [keyword]);
+    useEffect(() => {
+      const loading = setTimeout(() => {
+        if (keyword !== '' && !keyword.startsWith('')) {
+          Search();
+        }
+      }, 100);
 
-  return (
-    <StyledSearch>
-      <Header
-        type="search"
-        inputtype="text"
-        onChange={(e) => {
-          setKeyword(e.target.value);
-        }}
-      />
-      <main className="searchMain">
-        <h3 className="ir">검색어와 일치하는 사용자의 리스트</h3>
-        {searchList.map((item) => (
-          <UserList
-            searchList={searchList}
-            keyword={keyword}
-            key={item._id}
-            username={item.username}
-            accountname={`@ ${item.accountname}`}
-            image={item.image}
-            alt={`${item.username}님의 프로필 사진`}
-          />
-        ))}
-      </main>
-      <TabMenu></TabMenu>
-    </StyledSearch>
-  );
-}
+      return () => {
+        clearTimeout(loading);
+      };
+    }, [keyword]);
 
-export default Search;
+    return (
+      <StyledSearch>
+        <Header
+          type="search"
+          inputtype="text"
+          onChange={(e) => {
+            setKeyword(e.target.value);
+          }}
+        />
+        <main className="searchMain">
+          <h3 className="ir">검색어와 일치하는 사용자의 리스트</h3>
+          {searchList.map((item) => (
+            <UserList onclick={() => handleClick(item.accountname)}
+              searchList={searchList}
+              keyword={keyword}
+              key={item._id}
+              username={item.username}
+              accountname={`@ ${item.accountname}`}
+              image={item.image}
+              alt={`${item.username}님의 프로필 사진`}
+            />
+          ))}
+        </main>
+        <TabMenu></TabMenu>
+      </StyledSearch>
+    );
+  }
+
+  export default Search;

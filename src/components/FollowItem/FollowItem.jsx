5205cffa9eb44f94a3dae6b078448f1ac43815ea
beomkwan.choi <3031011@naver.com>
2023-06-25 21:43:13 +0900

    fix: 팔로우 상태, 토글 오류 수정 / 로그인 유저와 동일한 유저라면 버튼 숨김

--- src/components/FollowItem/FollowItem.jsx
+++ src/components/FollowItem/FollowItem.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import { Container, FollowerImgTest, FollowerInfo, FollowerName, FollowerIntro} from './FollowItemStyle'
+import { Container, FollowerImgTest, FollowerInfo, FollowerName, FollowerIntro } from "./FollowItemStyle";
 import Button from "../FollowButton/FollowButton";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-export default function FollowItem({ username, intro, image, accountname }) {
-  const [follow, setFollow] = useState(true);
+export default function FollowItem({ username, intro, image, accountname, isfollow }) {
+  const [follow, setFollow] = useState(isfollow);
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
+  const myUsername = localStorage.getItem("username");
 
   function moveProfile(accountname) {
     navigate(`/profile/${accountname}`, {
@@ -19,15 +20,12 @@ export default function FollowItem({ username, intro, image, accountname }) {
 
   const Follow = async () => {
     try {
-      const token = await axios.get(
-        `https://api.mandarin.weniv.co.kr/user/checktoken`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-type": "application/json",
-          },
-        },
-      );
+      // const token = await axios.get(`https://api.mandarin.weniv.co.kr/user/checktoken`, {
+      //   headers: {
+      //     Authorization: `Bearer ${token}`,
+      //     "Content-type": "application/json",
+      //   },
+      // });
       const res = await axios.post(
         `https://api.mandarin.weniv.co.kr/profile/${accountname}/follow`,
         {},
@@ -36,63 +34,41 @@ export default function FollowItem({ username, intro, image, accountname }) {
             Authorization: `Bearer ${token}`,
             "Content-type": "application/json",
           },
-        },
+        }
       );
-      setFollow(!follow);
+      setFollow(res.data.profile.isfollow);
     } catch (err) {
       console.error("에러!", err);
-      navigate("/error");
     }
   };
 
   const UnFollow = async () => {
     try {
-      const res = await axios.delete(
-        `https://api.mandarin.weniv.co.kr/profile/${accountname}/unfollow`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-type": "application/json",
-          },
+      const res = await axios.delete(`https://api.mandarin.weniv.co.kr/profile/${accountname}/unfollow`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-type": "application/json",
         },
-      );
-      setFollow(!follow);
+      });
+      setFollow(res.data.profile.isfollow);
     } catch (err) {
       console.error("에러!", err);
-      navigate("/error");
     }
   };
 
   return (
     <Container>
-      <FollowerImgTest
-        src={image}
-        alt="프로필 이미지"
-        onClick={() => moveProfile(accountname)}
-      />
+      <FollowerImgTest src={image} alt="프로필 이미지" onClick={() => moveProfile(accountname)} />
       <FollowerInfo onClick={() => moveProfile(accountname)}>
         <FollowerName>{username}</FollowerName>
         <FollowerIntro>{intro}</FollowerIntro>
       </FollowerInfo>
-      {!follow ? (
-        <Button
-          type="button"
-          content="팔로우"
-          width="s"
-          size="s"
-          bgColor="active"
-          onClick={Follow}
-        />
+      {myUsername === username ? (
+        <></>
+      ) : !follow ? (
+        <Button type="button" content="팔로우" width="s" size="s" bgColor="active" onClick={Follow} />
       ) : (
-        <Button
-          type="button"
-          content="취소"
-          width="s"
-          size="s"
-          border="active"
-          color="active"
-          onClick={UnFollow}
-        />
+        <Button type="button" content="취소" width="s" size="s" border="active" color="active" onClick={UnFollow} />
       )}
     </Container>
   );

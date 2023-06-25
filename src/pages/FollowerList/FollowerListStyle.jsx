d7ae3644270fbae825e1f38ff50dff4b6bec38cf
beomkwan.choi <3031011@naver.com>
2023-06-25 21:50:31 +0900

    docs: 팔로잉 팔로우 페이지 스타일 수정

--- src/pages/FollowerList/FollowerListStyle.jsx
+++ src/pages/FollowerList/FollowerListStyle.jsx
@@ -2,12 +2,11 @@ import styled from "styled-components";
 
 export const FollowList = styled.ul`
   width: 358px;
-  padding: 60px 16px 24px;
+  padding: 60px 16px 0;
   background-color: #fff;
-  height: 100vh;
+  margin-bottom: 80px;
 `;
 
 export const FollowListItem = styled.li`
   margin-bottom: 12px;
 `;
-

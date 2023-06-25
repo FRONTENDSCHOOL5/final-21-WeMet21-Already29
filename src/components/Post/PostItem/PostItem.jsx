61502963d09e1d5e96aa84622a70f271ca2246a0
beomkwan.choi <3031011@naver.com>
2023-06-25 15:39:40 +0900

    docs: 홈피드 유저 게시글, 프로필 사진 이미지 에러 함수 전달

--- src/components/Post/PostItem/PostItem.jsx
+++ src/components/Post/PostItem/PostItem.jsx
@@ -4,6 +4,7 @@ import { Container, TextComment, PostUser, PostUserImg, PostUserBox, PostUserNam
 import heart from "../../../assets/images/uil_heart.png";
 import fillHeart from "../../../assets/images/uil_fullHeart.png";
 import { heartButtonHandler } from "../../../utils/heartButtonHandler";
+import { imageErrorHandler, profileImgErrorHandler } from "../../../utils/imageErrorHandler";
 
 export default function PostItem({ modalOpen, myFeed }) {
   function formatDate(dateString) {
@@ -14,19 +15,19 @@ export default function PostItem({ modalOpen, myFeed }) {
 
     return `${year}년 ${month}월 ${day}일`;
   }
-  console.log(myFeed)
+  console.log(myFeed);
 
-const heartHandler = async (postId, postHeart) => {
-  if (postHeart) {
-    const res = await heartButtonHandler.minus(postId);
-    const json = await res.json();
-    console.log(json);
-  } else {
-    const res = await heartButtonHandler.plus(postId);
-    const json = await res.json();
-    console.log(json);
-  }
-  }; 
+  const heartHandler = async (postId, postHeart) => {
+    if (postHeart) {
+      const res = await heartButtonHandler.minus(postId);
+      const json = await res.json();
+      console.log(json);
+    } else {
+      const res = await heartButtonHandler.plus(postId);
+      const json = await res.json();
+      console.log(json);
+    }
+  };
 
   return (
     <>
@@ -34,34 +35,36 @@ const heartHandler = async (postId, postHeart) => {
         myFeed.map((item) => (
           <Container key={item.id}>
             <PostUser to={`/profile/${item.author.accountname}`}>
-              <PostUserImg src={item.author.image} alt="사용자 이미지" />
+              <PostUserImg src={item.author.image} alt="사용자 이미지" onError={profileImgErrorHandler} />
               <PostUserBox>
                 <PostUserName>{item.author.username}</PostUserName>
                 <PostUserId>@ {item.author.accountname}</PostUserId>
               </PostUserBox>
             </PostUser>
             <PostContent>
-            <TextComment>{item.content}</TextComment>
-            {item.author.image && <PostImg src={item.author.image} alt="포스트 이미지" />}
-            <PostInfoBox>
+              <TextComment>{item.content}</TextComment>
+              {item.author.image && <PostImg src={item.author.image} alt="포스트 이미지" onError={imageErrorHandler} />}
+              <PostInfoBox>
                 <PostBtnBox>
-                <BtnLike onClick={() => {
-                    heartHandler(item.id, item.hearted);
-                  }}>
-                  <BtnImg src={item.hearted ? fillHeart : heart} className="heart-image" alt="게시글 좋아요" />
-                  {item.heartCount}
-                </BtnLike>
-                <BtnComment>
-                  <BtnImg src={require("../../../assets/images/icon-message-circle-1.svg").default} alt="게시글 댓글" />
-                  {item.commentCount}
-                </BtnComment>
-              </PostBtnBox>
-              <PostDate>{formatDate(item.createdAt)}</PostDate>
-            </PostInfoBox>
-          </PostContent>
-          <BtnMore onClick={modalOpen}></BtnMore>
-        </Container>
-      ))}
+                  <BtnLike
+                    onClick={() => {
+                      heartHandler(item.id, item.hearted);
+                    }}
+                  >
+                    <BtnImg src={item.hearted ? fillHeart : heart} className="heart-image" alt="게시글 좋아요" />
+                    {item.heartCount}
+                  </BtnLike>
+                  <BtnComment>
+                    <BtnImg src={require("../../../assets/images/icon-message-circle-1.svg").default} alt="게시글 댓글" />
+                    {item.commentCount}
+                  </BtnComment>
+                </PostBtnBox>
+                <PostDate>{formatDate(item.createdAt)}</PostDate>
+              </PostInfoBox>
+            </PostContent>
+            <BtnMore onClick={modalOpen}></BtnMore>
+          </Container>
+        ))}
     </>
   );
 }

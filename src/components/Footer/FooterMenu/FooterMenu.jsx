dbf74637f1aa03ae87ccd364ac77f9397f64bac3
beomkwan.choi <3031011@naver.com>
2023-06-25 15:20:49 +0900

    fix: import 구문 오류 해결

--- src/components/Footer/FooterMenu/FooterMenu.jsx
+++ src/components/Footer/FooterMenu/FooterMenu.jsx
@@ -7,88 +7,50 @@ import editIcon from "../../../assets/images/IconEdit.png";
 import fillEditIcon from "../../../assets/images/FillIconEdit.png";
 import userIcon from "../../../assets/images/IconUser.png";
 import fillUserIcon from "../../../assets/images/FillIconUser.png";
-import { NavWrapper, NavLink, StyledNavText } from './FooterMenuStyle';
-import { useEffect } from "react";
+import { NavWrapper, NavLink, StyledNavText } from "./FooterMenuStyle";
+import { useEffect, useState } from "react";
+import { LinkStyle } from "../../Profile/ProfileProduct/ProfileProductStyle";
 
 export default function Navigation() {
   const location = useLocation();
   const [accountname, setAccountname] = useState("");
 
   const fetchUserInfo = () => {
-    fetch("https://api.mandarin.weniv.co.kr/user/myinfo",
-      {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${localStorage.getItem("token")}`
-        },
-      })
-      .then(res => res.json())
-      .then(json => setAccountname(json.user.accountname));
-    
-    useEffect(() => {
-      fetchUserInfo()
-    }, []);
+    fetch("https://api.mandarin.weniv.co.kr/user/myinfo", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => setAccountname(json.user.accountname));
   };
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, []);
 
   return (
     <NavWrapper>
-      <NavLink
-        to="/home"
-        className={`nav-link ${location.pathname === "/home" ? "active" : ""}`}
-      >
-        <img
-          src={location.pathname === "/home" ? fillHomeIcon : homeIcon}
-          alt="홈"
-          width="24px"
-        />
+      <NavLink to="/home" className={`nav-link ${location.pathname === "/home" ? "active" : ""}`}>
+        <img src={location.pathname === "/home" ? fillHomeIcon : homeIcon} alt="홈" width="24px" />
         <StyledNavText>홈</StyledNavText>
       </NavLink>
 
-      <NavLink
-        to="/search"
-        className={`nav-link ${
-          location.pathname === "/search" ? "active" : ""
-        }`}
-      >
-        <img
-          src={location.pathname === "/search" ? fillSearchIcon : searchIcon}
-          alt="검색"
-          width="24px"
-        />
+      <NavLink to="/search" className={`nav-link ${location.pathname === "/search" ? "active" : ""}`}>
+        <img src={location.pathname === "/search" ? fillSearchIcon : searchIcon} alt="검색" width="24px" />
         <StyledNavText>검색</StyledNavText>
       </NavLink>
 
-      <NavLink
-        to="/post/upload"
-        className={`nav-link ${
-          location.pathname === "/post/upload" ? "active" : ""
-        }`}
-      >
-        <img
-          src={location.pathname === "/post/upload" ? fillEditIcon : editIcon}
-          alt="작성"
-          width="24px"
-        />
+      <NavLink to="/post/upload" className={`nav-link ${location.pathname === "/post/upload" ? "active" : ""}`}>
+        <img src={location.pathname === "/post/upload" ? fillEditIcon : editIcon} alt="작성" width="24px" />
         <StyledNavText>게시물 작성</StyledNavText>
       </NavLink>
 
-      <LinkStyle
-        to={`/profile/${accountname}`}
-        className={`nav-link ${
-          location.pathname === "/profile/${accountname}" ? "active" : ""
-        }`}
-      >
-        <img
-          src={
-            location.pathname === "/profile/${accountname}"
-              ? fillUserIcon
-              : userIcon
-          }
-          alt="프로필"
-          width="24px"
-        />
+      <NavLink to={`/profile/${accountname}`} className={`nav-link ${location.pathname === `/profile/${accountname}` ? "active" : ""}`}>
+        <img src={location.pathname === `/profile/${accountname}` ? fillUserIcon : userIcon} alt="프로필" width="24px" />
         <StyledNavText>프로필</StyledNavText>
-      </LinkStyle>
+      </NavLink>
     </NavWrapper>
   );
 }

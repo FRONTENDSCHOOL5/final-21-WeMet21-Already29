d3fa3fb1bfc0eae1614a243533767a4bec0837b2
beomkwan.choi <3031011@naver.com>
2023-06-27 02:39:34 +0900

    fix: 다른 유저 프로필에서 내 프로필로 이동했을 시 오류 해결

--- src/components/Profile/ProfilePost/ProfilePost.jsx
+++ src/components/Profile/ProfilePost/ProfilePost.jsx
@@ -11,14 +11,10 @@ export default function ProfilePost() {
   const [posts, setPosts] = useState(null);
   const params = useParams();
   const pageEnd = useRef(null);
+  const myAccountname = localStorage.getItem("accountname");
   const { getData, page } = useInfiniteScroll(`post/${params.id}/userpost`, pageEnd);
 
-  useEffect(() => {
-    if (posts && posts.length !== 0 && posts[0].author.accountname !== params.id) {
-      setPosts(null);
-      console.log("포스트 리스트 모두 지우기");
-    }
-
+  const fetchPost = () => {
     getData(page)
       .then((res) => res.json())
       .then((json) =>
@@ -30,15 +26,38 @@ export default function ProfilePost() {
           }
         })
       );
+  };
+
+  useEffect(() => {
+    // 게시글이 있는 다른 프로필에서 내 프로필로 넘어올 때,
+    // 이전에 있던 포스트 데이터의 작성자가 로그인한 유저가 아닐 때
+    if (posts && posts.length !== 0 && posts[0].author.accountname !== params.id) {
+      console.log("case0");
+      // 이전 포스트 데이터 삭제
+      setPosts(null);
+      getData(0)
+        .then((res) => res.json())
+        .then((json) => setPosts(json.post));
+    }
+    // 게시글이 없는 다른 프로필에서 내 프로필로 넘어올 때
+    else if (posts && posts.length === 0 && myAccountname === params.id) {
+      console.log("case1");
+      getData(0)
+        .then((res) => res.json())
+        .then((json) => setPosts(json.post));
+    }
+    // 다른 페이지에서 내 프로필로 넘어올 때
+    else if (myAccountname === params.id) {
+      console.log("case2");
+      fetchPost();
+    }
+    // 로그인 한 유저의 프로필이 아닐 때
+    else {
+      console.log("case3");
+      fetchPost();
+    }
   }, [page, params]);
 
-  // useEffect(() => {
-  //   setPosts(null);
-
-  //   console.log("파라미터가 바꼈을 시에만 첫 포스트들 불러오기");
-  // }, [params]);
-
-  console.log(posts);
   return (
     <>
       {posts && posts.length !== 0 && (

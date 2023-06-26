b0c92ebcdc8c634c6b1928aca02db3022dcdbd74
beomkwan.choi <3031011@naver.com>
2023-06-27 02:51:39 +0900

    docs: splash 페이지 디자인 및 애니메이션 변경 완료

--- src/pages/Splash/SplashPage.jsx
+++ src/pages/Splash/SplashPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import mainLogo from "../../assets/images/main-logo.png";
 import bigEarth from "../../assets/images/bigEarth.png";
+
 import { StyledSplashPage, EarthImageWrap, LogoImageWrap } from "./SplashStyle";
 
 const SplashPage = () => (
@@ -9,6 +10,7 @@ const SplashPage = () => (
       <img src={mainLogo} alt="mainLogo" className="main-logo" />
     </LogoImageWrap>
     <EarthImageWrap>
+      <div className="walk-mascot" />
       <img src={bigEarth} alt="footerLogo" className="earth-image" />
     </EarthImageWrap>
   </StyledSplashPage>
--- src/pages/Splash/SplashStyle.jsx
+++ src/pages/Splash/SplashStyle.jsx
@@ -1,47 +1,58 @@
 import styled, { keyframes } from "styled-components";
+import walkMascot from "../../assets/images/walkMascot.png";
 
 const fadeInOut = keyframes`
     from {opacity: .2;} 
 `;
 
-const Earth = keyframes`
-  from{
-    transform: translateY(40px);
-    opacity: .5
-  }
-
-`;
-
 const rotateEarth = keyframes`
-  to{
-    rotate: 360deg;
-  }
+  to{rotate: -360deg}
 `;
-
-const EarthImageWrap = styled.div``;
-const LogoImageWrap = styled.div``;
 
 const StyledSplashPage = styled.main`
+  position: relative;
   background-color: white;
   box-shadow: -1px 0 30px -1px #f2f2f2, 1px 0 30px -1px #f2f2f2;
+  padding-top: 20%;
   height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-end;
   overflow: hidden;
+  box-sizing: border-box;
+`;
+
+const LogoImageWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 10%;
 
   .main-logo {
     animation: ${fadeInOut} ease-out 0.6s;
     animation: name duration timing-function delay iteration-count direction fill-mode;
-    margin-right: 38px;
   }
+`;
+
+const EarthImageWrap = styled.div`
+  width: 200%;
+  position: relative;
+  bottom: -50%;
+  left: -50%;
 
   .earth-image {
-    animation: ${Earth} 0.4s ease-in-out, ${rotateEarth} 10s cubic-bezier(0, 0, 1, 1) 0.4s infinite;
-    animation: name duration timing-function delay iteration-count direction fill-mode;
-    position: relative;
-    bottom: -60%;
+    animation: ${rotateEarth} 8s cubic-bezier(0, 0, 1, 1) infinite;
+  }
+  .walk-mascot {
+    position: absolute;
+    top: -20%;
+    left: 30%;
+    z-index: 1;
+    width: calc(2049px / 8);
+    height: 170px;
+    background: url(${walkMascot}) no-repeat 0 0 / auto 100%;
+    animation: walk 1s infinite steps(7);
+  }
+  @keyframes walk {
+    to {
+      background-position: right 0;
+    }
   }
 `;
 

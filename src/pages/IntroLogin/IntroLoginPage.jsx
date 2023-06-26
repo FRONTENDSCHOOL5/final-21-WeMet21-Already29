5c91c185b7546b3b19af45cdd95a1183b3ac7bc7
BK <3031011@naver.com>
2023-06-26 17:57:33 +0900

    refactor: 로그인 인트로 페이지 마크업 및 css 수정 중

--- src/pages/IntroLogin/IntroLoginPage.jsx
+++ src/pages/IntroLogin/IntroLoginPage.jsx
@@ -1,41 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import charaterLogo from '../../assets/images/character-logo.png'
-import StyledIntroLoginPage from './IntroLoginStyle';
+import React from "react";
+import charaterLogo from "../../assets/images/mascot.png";
+import { EmailLoginLink, EmailSignUpLink, StyledIntroLoginPage } from "./IntroLoginStyle";
 
 function IntroLoginPage() {
   return (
     <StyledIntroLoginPage>
-        <section>
-          <h1>입9팔9</h1>
-          <p className='first-p'>데일리룩과 취향을 공유하며 </p>
-          <p className='second-p'>지속가능한 패션을 함께 만들어가는 공간 </p>
-        </section>
-
-        <section>
-          <img src={charaterLogo} alt="charaterLogo" className='character-logo'/>
-        </section>
-        
-        <section className="login-join-section">
-          <ul className="sns-login">
-            <li className="sns-login-li">
-              <Link to="/login" className='email-text'>
-                <button className="email-login">이메일로 로그인</button>
-              </Link> 
-            </li>
-          </ul>
-          <ul className="login-join-cont">
-            <li>
-              <p className="emailLogin">처음오셨나요?</p>
-            </li>
-            <li>
-              <Link to="/signup" className="emailJoin">이메일로 가입하기</Link>
-            </li>
-          </ul>
-        </section>
+      <h1>입9팔9</h1>
+      <p className="sub-title">데일리룩과 취향을 공유하며 지속가능한 패션을 함께 만들어가는 공간</p>
+      <img src={charaterLogo} alt="입구팔구 마스코트 캐릭터 사진" />
+      <EmailLoginLink to="login">이메일로 로그인</EmailLoginLink>
+      <p className="signup-text">
+        <span>처음오셨나요?</span>
+        <EmailSignUpLink to="signup">이메일로 가입하기</EmailSignUpLink>
+      </p>
     </StyledIntroLoginPage>
   );
 }
 
 export default IntroLoginPage;
-
--- src/pages/IntroLogin/IntroLoginStyle.jsx
+++ src/pages/IntroLogin/IntroLoginStyle.jsx
@@ -1,169 +1,61 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import emailImage from "../../assets/images/mail.png";
 
 const StyledIntroLoginPage = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   height: 100vh;
-  width: 390px;
-  position: relative;
-  box-shadow: -1px 0 30px -1px #f2f2f2, 1px 0 30px -1px #f2f2f2;
-  margin: 0 auto;
+  gap: 10px;
+  font-size: 1.6rem;
+  box-shadow: 1px 1px 10px black;
+  padding: 0 34px;
 
   h1 {
-    position: absolute;
-    width: 127px;
-    height: 30px;
-    left: 34px;
-    top: 187px;
-
-    font-family: "Noto Sans KR";
-    font-style: normal;
+    font-size: 3rem;
     font-weight: 700;
-    font-size: 40px;
-    line-height: 30px;
-
-    display: flex;
-    align-items: flex-end;
+    align-self: flex-start;
   }
 
-  .first-p {
-    position: absolute;
-    width: 291px;
-    height: 50px;
-    left: 34px;
-    top: 242px;
-
-    font-family: "Noto Sans KR";
-    font-style: normal;
+  .sub-title {
+    font-size: 1.8rem;
     font-weight: 400;
-    font-size: 18px;
-    line-height: 25px;
+  }
 
+  img {
+    margin: 60px 0 30px;
+  }
+  .signup-text {
     display: flex;
-    align-items: flex-end;
-    letter-spacing: -0.1em;
-    text-overflow: ellipsis;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 3;
-  }
-
-  .second-p {
-    position: absolute;
-    width: 291px;
-    height: 77px;
-    left: 34px;
-    top: 242px;
-
-    font-family: "Noto Sans KR";
-    font-style: normal;
-    font-weight: 350;
-    font-size: 18px;
-    line-height: 25px;
-
-    display: flex;
-    align-items: flex-end;
-    letter-spacing: -0.1em;
-    text-overflow: ellipsis;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 3;
-  }
-
-  .character-logo {
-    position: absolute;
-    width: 322px;
-    height: 237.74px;
-    left: calc(50% - 322px / 2);
-    top: calc(50% - 237.74px / 2 + 134.87px);
-  }
-
-  .login-join-section {
     width: 100%;
-    position: absolute;
-    bottom: 0;
-    border-top-left-radius: 20px;
-    border-top-right-radius: 20px;
-    padding: 63px 4px 84px;
-    background-color: #ffffff;
-  }
-
-  .sns-login-li img {
-    width: 18px;
-    height: 18px;
-    background-image: url("");
-  }
-
-  .sns-login-li {
-    display: flex;
-    justify-content: center;
-  }
-
-  .sns-login-li button {
-    cursor: pointer;
-  }
-
-  .sns-login-li button::before {
-    content: "";
-  }
-
-  .email-login {
-    border: 1px solid #0b0b0b;
-    border-radius: 10px;
-    width: 322px;
-    height: 44px;
-    margin-bottom: 10px;
-    background-position: 10px 8px;
-    cursor: pointer;
-    text-align: center;
-  }
-
-  .email-login:hover {
-    display: block;
-    background-color: rgba(5, 139, 46, 1);
-    color: white;
-  }
-
-  .email-text {
-    text-align: center;
-    margin-left: 10px;
-
-    font-family: "Noto Sans KR";
-    font-style: normal;
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 23px;
-  }
-
-  .login-join-cont {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-  }
-
-  .join {
-    font-weight: bold;
-  }
-
-  .emailJoin {
-    text-align: center;
-    margin-left: 10px;
-    font-family: "Noto Sans KR";
-    font-style: normal;
-    font-weight: 700;
-    font-size: 16px;
-  }
-
-  .emailLogin {
-    font-family: "Noto Sans KR";
-    font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 23px;
-  }
-
-  .emailLogin::after {
-    content: "";
-    width: 1px;
-    margin: 0 12px;
+    justify-content: space-between;
   }
 `;
 
-export default StyledIntroLoginPage;
+const EmailLoginLink = styled(Link)`
+  display: block;
+  border: 1px solid #ccc;
+  width: 80%;
+  text-align: center;
+  padding: 10px 0;
+  border-radius: 10px;
 
+  &::before {
+    content: url(${emailImage});
+    width: 12px;
+    height: 12px;
+  }
+`;
+
+const EmailSignUpLink = styled(Link)`
+  font-weight: 700;
+  &::after {
+    content: ">>";
+    font-size: 1.4rem;
+    margin-left: 8px;
+  }
+`;
+
+export { StyledIntroLoginPage, EmailSignUpLink, EmailLoginLink };

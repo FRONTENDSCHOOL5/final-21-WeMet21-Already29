00c1ad495bd1b277d8aedd4abde5dee9709913a3
yonainthefish <yonaisgood@gmail.com>
2023-07-21 09:53:01 +0900

    refector: 검색 페이지 Card 컴포넌트 활용 리팩토링

--- src/pages/Search/SearchPage.jsx
+++ src/pages/Search/SearchPage.jsx
@@ -1,87 +1,84 @@
-import React, { useEffect, useState } from 'react';
-import StyledSearch from './SearchPageStyle'
-import Header from '../../components/Header/Header';
-import TabMenu from '../../components/Footer/FooterMenu/FooterMenu';
-import UserList from '../../components/UserList/UserList';
+import React, { useEffect, useState } from "react";
+import StyledSearch from "./SearchPageStyle";
+import Header from "../../components/Header/Header";
+import TabMenu from "../../components/Footer/FooterMenu/FooterMenu";
+import CardHeader from "../../components/Card/CardHeader/CardHeader";
 import { useNavigate } from "react-router-dom";
 
 function Search() {
-    const navigate = useNavigate();
-    const [searchList, setSearchList] = useState([]);
-    const [keyword, setKeyword] = useState('');
-  
-    function handleClick(accountname) {
-      console.log(accountname);
-      navigate(`/profile/${accountname}`, {
-        state: {
-          accountname: accountname,
-        },
-      });
-    }
+  const navigate = useNavigate();
+  const [searchList, setSearchList] = useState([]);
+  const [keyword, setKeyword] = useState("");
 
-    useEffect(() => {
-      if (keyword) {
-        const GetUserInfo = async () => {
-          try {
-            const response = await fetch(
-              `https://api.mandarin.weniv.co.kr/user/searchuser/?keyword=${keyword}`,
-              {
-                method: 'GET',
-                headers: {
-                  Authorization: `Bearer ${localStorage.getItem("token")}`,
-                  'Content-type': 'application/json',
-                },
-              }
-            );
-            const result = await response.json();
-
-            setSearchList(result);
-          } catch (err) {
-            console.log(err);
-          }
-        };
-        GetUserInfo();
-      }
-    }, [keyword]);
-
-    useEffect(() => {
-      const loading = setTimeout(() => {
-        if (keyword !== '' && !keyword.startsWith('')) {
-          Search();
-        }
-      }, 100);
-
-      return () => {
-        clearTimeout(loading);
-      };
-    }, [keyword]);
-
-    return (
-      <StyledSearch>
-        <Header
-          type="search"
-          inputtype="text"
-          onChange={(e) => {
-            setKeyword(e.target.value);
-          }}
-        />
-        <main className="searchMain">
-          <h3 className="ir">검색어와 일치하는 사용자의 리스트</h3>
-          {searchList.map((item) => (
-            <UserList onclick={() => handleClick(item.accountname)}
-              searchList={searchList}
-              keyword={keyword}
-              key={item._id}
-              username={item.username}
-              accountname={`@ ${item.accountname}`}
-              image={item.image}
-              alt={`${item.username}님의 프로필 사진`}
-            />
-          ))}
-        </main>
-        <TabMenu></TabMenu>
-      </StyledSearch>
-    );
+  function handleClick(accountname) {
+    console.log(accountname);
+    navigate(`/profile/${accountname}`, {
+      state: {
+        accountname: accountname,
+      },
+    });
   }
 
-  export default Search;
+  useEffect(() => {
+    if (keyword) {
+      const GetUserInfo = async () => {
+        try {
+          const response = await fetch(
+            `https://api.mandarin.weniv.co.kr/user/searchuser/?keyword=${keyword}`,
+            {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-type": "application/json",
+              },
+            }
+          );
+          const result = await response.json();
+
+          setSearchList(result);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      GetUserInfo();
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    const loading = setTimeout(() => {
+      if (keyword !== "" && !keyword.startsWith("")) {
+        Search();
+      }
+    }, 100);
+
+    return () => {
+      clearTimeout(loading);
+    };
+  }, [keyword]);
+
+  return (
+    <StyledSearch>
+      <Header
+        type="search"
+        inputtype="text"
+        onChange={(e) => {
+          setKeyword(e.target.value);
+        }}
+      />
+      <main className="searchMain">
+        <h3 className="a11y-hidden">검색어와 일치하는 사용자의 리스트</h3>
+        {searchList.map((item) => (
+          <CardHeader
+            image={item.image}
+            username={item.username}
+            accountname={item.accountname}
+            key={item._id}
+          />
+        ))}
+      </main>
+      <TabMenu></TabMenu>
+    </StyledSearch>
+  );
+}
+
+export default Search;

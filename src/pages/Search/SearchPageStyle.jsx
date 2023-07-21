3879a60ab672232013efd9e63bfc5f128579a1b8
yonainthefish <yonaisgood@gmail.com>
2023-07-21 09:54:05 +0900

    docs: 검색 페이지 스타일 수정

--- src/pages/Search/SearchPageStyle.jsx
+++ src/pages/Search/SearchPageStyle.jsx
@@ -1,26 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledSearch = styled.section`
-  width: 390px;
+  width: 39rem;
   height: 100vh;
   position: relative;
   margin: 0 auto;
-  background-color: white;
-
-  * {
-    box-sizing: border-box;
-  }
-
-  h3 {
-    font-size: var(--font-lg-size);
-  }
 
   .searchMain {
     display: flex;
     flex-direction: column;
-    height: 90vh;
+    height: 100vh;
     gap: 16px;
-    padding: 20px 16px;
+    padding: 2rem 1.6rem;
     overflow: scroll;
     ::-webkit-scrollbar {
       display: none;

af3e3b3c4787d8f54719b4cdec0aaeb77b4a52ca
beomkwan.choi <3031011@naver.com>
2023-07-24 16:58:36 +0900

    refactor: 데이터 업로드 시간 구하는 함수 수정

--- src/utils/uploadDateCalculate.js
+++ src/utils/uploadDateCalculate.js
@@ -1,38 +1,67 @@
-export default function uploadDateCalculate(updateDate) {
-  const curr = new Date();
-  const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
-  const koreaTimeDiff = 9 * 60 * 60 * 1000;
-  const nowKoreaTime = new Date(utc + koreaTimeDiff).toISOString();
-  const nowDateArray = nowKoreaTime.split("T")[0].split("-");
-  const nowTimeArray = nowKoreaTime.split("T")[1].slice(0, 8).split(":");
-  // 유저가 게시글을 업데이트 한 [년, 월, 일]순으로 배열에 저장됨
-  const updateDateArray = updateDate.split("T")[0].split("-");
-  // 유저가 게시글을 업데이트 한 [시, 분, 초]순으로 배열에 저장됨
-  const updateTimeArray = updateDate.split("T")[1].slice(0, 8).split(":");
-  // 같은 년, 월, 일에 업데이트 되었는지 확인
-  if (nowDateArray[0] === updateDateArray[0] && nowDateArray[1] === updateDateArray[1] && nowDateArray[2] === updateDateArray[2]) {
-    // 같은 년, 월, 일에 업데이트 되었다면 시간, 분이 같은지 확인
-    if (nowTimeArray[0] === updateTimeArray[0] && nowTimeArray[1] === updateTimeArray[1]) {
-      // 같다면 몇초전인지 출력
-      return nowTimeArray[2] - updateTimeArray[2] + "초 전";
-      // 시간이 같은지 확인
-    } else if (nowTimeArray[0] === updateTimeArray[0]) {
-      // 같다면 몇분전인지 출력
-      return nowTimeArray[1] - updateTimeArray[1] + "분 전";
-    } else {
-      // 모두 다르면 몇시간 전인지 출력
-      return nowTimeArray[0] - updateTimeArray[0] + "시간 전";
-    }
-    // 같은 년, 월에 업데이트 되었는지 확인
-  } else if (nowDateArray[0] === updateDateArray[0] && nowDateArray[1] === updateDateArray[1]) {
-    // 같은 년, 월에 업데이트 되었다면 몇일전인지 출력
-    return nowDateArray[2] - updateDateArray[2] + "일 전";
-    // 같은 년에 업데이트 되었는지 확인
-  } else if (nowDateArray[0] === updateDateArray[0]) {
-    // 같은 년에 업데이트 되었다면 몇달전인지 출력
-    return nowDateArray[1] - updateDateArray[1] + "달 전";
-  } else {
-    // 모든 조건이 부합하지 않으면 몇년전인지 출력
-    return nowDateArray[0] - updateDateArray[0] + "년 전";
+export default function uploadDateCalculate(dataUpdatedAt) {
+  function parseDateTime(dateTime) {
+    const dateArray = dateTime.split("T")[0].split("-");
+    const timeArray = dateTime.split("T")[1].slice(0, 8).split(":");
+    return {
+      year: dateArray[0],
+      month: dateArray[1],
+      date: dateArray[2],
+      hour: timeArray[0],
+      min: timeArray[1],
+      sec: timeArray[2],
+    };
   }
+
+  const convertToKoreaTime = (time) => {
+    const utc = time.getTime() + time.getTimezoneOffset() * 60 * 1000;
+    const koreaTimeDiff = 9 * 60 * 60 * 1000;
+    return new Date(utc + koreaTimeDiff).toISOString();
+  };
+
+  const nowKoreaTime = convertToKoreaTime(new Date());
+  const nowDateData = parseDateTime(nowKoreaTime);
+  const updatedDateData = parseDateTime(dataUpdatedAt);
+
+  let result;
+
+  // 같은 년, 월, 일에 업데이트되었는지 확인
+  const isSameDate = Object.entries(nowDateData)
+    .slice(0, 3)
+    .every(([key, value]) => updatedDateData[key] === value);
+
+  console.log(nowDateData, updatedDateData);
+
+  if (isSameDate) {
+    const { hour: nowHour, min: nowMin, sec: nowSec } = nowDateData;
+    const { hour: updateHour, min: updateMin, sec: updateSec } = updatedDateData;
+
+    const diffSec = nowMin === updateMin ? nowSec - updateSec : 60 - Number(updateSec) + Number(nowSec);
+    const diffMin = nowHour + nowMin - (updateHour + updateMin);
+    const diffHour = parseInt(diffMin / 60);
+
+    if (diffHour === 0) {
+      if (diffMin === 0) {
+        diffSec < 5 ? (result = "방금 전") : (result = diffSec + "초 전");
+      } else {
+        result = diffMin + "분 전";
+      }
+    } else {
+      result = diffHour + "시간 전";
+    }
+  } else {
+    const { year: updateYear, month: updateMonth, date: updateDate } = updatedDateData;
+    const { year: nowYear, month: nowMonth, date: nowDate } = nowDateData;
+
+    if (nowYear === updateYear) {
+      if (nowMonth === updateMonth) {
+        result = nowDate - updateDate + "일 전";
+      } else {
+        result = nowMonth - updateMonth + "달 전";
+      }
+    } else {
+      result = nowYear - updateYear + "년 전";
+    }
+  }
+
+  return result;
 }

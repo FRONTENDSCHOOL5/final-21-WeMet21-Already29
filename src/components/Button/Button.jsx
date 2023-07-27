8dae68e8717cda8ac36d325874763e6a92ca5702
beomkwan.choi <3031011@naver.com>
2023-07-27 16:57:29 +0900

    fix: 컴포넌트 수정

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleButton, ProfileNav, WhiteButton } from "./ButtonStyle";
 
-export default function Button({ category, type, width, height, disabled, children, fontSize, ...props }) {
+export default function Button({ category, type, width, height, to, disabled, children, fontSize, ...props }) {
   const button = {
     basic: (
       <StyleButton type={type} width={width} height={height} disabled={disabled} fontSize={fontSize} {...props}>
@@ -13,7 +13,7 @@ export default function Button({ category, type, width, height, disabled, childr
         {children}
       </WhiteButton>
     ),
-    profileNav: <ProfileNav to="hello">{children}</ProfileNav>,
+    profileNav: <ProfileNav to={to}>{children}</ProfileNav>,
   };
 
   return button[category];

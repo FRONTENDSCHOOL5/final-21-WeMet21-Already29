72cfbe321e0df8808ebf99349e9d923bb84b11e7
BK <3031011@naver.com>
2023-07-29 17:44:32 +0900

    fix: 비동기통신 예외처리

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,12 +1,18 @@
 export default async function fetchApi(fetchUrl, fetchMethod, body) {
-  const res = await fetch(`https://api.mandarin.weniv.co.kr/${fetchUrl}`, {
-    method: fetchMethod,
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-      "Content-type": "application/json",
-    },
-    body: body,
-  });
-  const json = res.json();
-  return json;
+  try {
+    const res = await fetch(`https://api.mandarin.weniv.co.kr/${fetchUrl}`, {
+      method: fetchMethod,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-type": "application/json",
+      },
+      body: body,
+    });
+
+    const json = res.json();
+
+    return json;
+  } catch (e) {
+    console.error(e);
+  }
 }

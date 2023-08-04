696e3c0b5cb872bcef9d8bb42dc034916fb6fdfa
beomkwan.choi <3031011@naver.com>
2023-08-04 18:49:03 +0900

    refactor: Router 내부 Context API Providers 분리

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -16,47 +16,57 @@ import Home from "../pages/HomeFeed/HomeFeed";
 import SignUpProfile from "../pages/SignUp/SignUpProfile/SignUpProfile";
 import Error from "../pages/404/Error";
 import FollowerList from "../pages/FollowerList/FollowerList";
+import UserInfo from "../contexts/UserInfo";
 
-function Router() {
+const Providers = ({ children }) => {
   const [isBottomSheetOpen, setBottomSheetOpen] = useState(false);
   const [isModalOpen, setModalOpen] = useState(false);
+  const [userInfo, setUserInfo] = useState(null);
 
   return (
     <BottomSheetContext.Provider value={{ isBottomSheetOpen, setBottomSheetOpen }}>
       <ModalContext.Provider value={{ isModalOpen, setModalOpen }}>
-        <Routes>
-          <Route path="/" element={<StartSplash />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/search" element={<SearchPage />}></Route>
-
-          <Route path="/profile/*" element={<Outlet />}>
-            <Route path=":id/*" element={<Outlet />}>
-              <Route path="" element={<Profile />} />
-              <Route path="following" element={<FollowerList followType="followingList" />} />
-              <Route path="follower" element={<FollowerList followType="followerList" type="followers" />} />
-              <Route path="modify" element={<SignUpProfile />} />
-            </Route>
-          </Route>
-
-          <Route path="/product/*" element={<Outlet />}>
-            <Route path="detail/:id" element={<ProductDetail />}></Route>
-            <Route path="upload" element={<UploadProduct />}></Route>
-            <Route path="modify/:id" element={<UploadProduct />}></Route>
-            <Route path="list/:id" element={<ProductList />}></Route>
-          </Route>
-
-          <Route path="post/*" element={<Outlet />}>
-            <Route path=":id" element={<PostDetail />}></Route>
-            <Route path="upload" element={<PostUpload />}></Route>
-            <Route path="modify/:id" element={<PostUpload />}></Route>
-          </Route>
-
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <UserInfo.Provider value={{ userInfo, setUserInfo }}>{children}</UserInfo.Provider>
       </ModalContext.Provider>
     </BottomSheetContext.Provider>
+  );
+};
+
+function Router() {
+  return (
+    <Providers>
+      <Routes>
+        <Route path="/" element={<StartSplash />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/search" element={<SearchPage />}></Route>
+
+        <Route path="/profile/*" element={<Outlet />}>
+          <Route path=":id/*" element={<Outlet />}>
+            <Route path="" element={<Profile />} />
+            <Route path="following" element={<FollowerList followType="followingList" />} />
+            <Route path="follower" element={<FollowerList followType="followerList" type="followers" />} />
+            <Route path="modify" element={<SignUpProfile />} />
+          </Route>
+        </Route>
+
+        <Route path="/product/*" element={<Outlet />}>
+          <Route path="detail/:id" element={<ProductDetail />}></Route>
+          <Route path="upload" element={<UploadProduct />}></Route>
+          <Route path="modify/:id" element={<UploadProduct />}></Route>
+          <Route path="list/:id" element={<ProductList />}></Route>
+        </Route>
+
+        <Route path="post/*" element={<Outlet />}>
+          <Route path=":id" element={<PostDetail />}></Route>
+          <Route path="upload" element={<PostUpload />}></Route>
+          <Route path="modify/:id" element={<PostUpload />}></Route>
+        </Route>
+
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Providers>
   );
 }
 

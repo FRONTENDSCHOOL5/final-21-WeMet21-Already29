5c6eab1ea4b2536910bd49eb80bfdae788f387d0
beomkwan.choi <3031011@naver.com>
2023-08-05 00:40:29 +0900

    refactor: fetch 유틸 함수 활용 리팩토링

--- src/components/Profile/ProfileProduct/ProfileProduct.jsx
+++ src/components/Profile/ProfileProduct/ProfileProduct.jsx
@@ -2,28 +2,15 @@ import React, { useEffect, useState } from "react";
 import Products from "../../Products/Products";
 import { LinkStyle, ProductSection } from "./ProfileProductStyle";
 import { useParams } from "react-router-dom";
+import fetchApi from "../../../utils/fetchApi";
 
 export default function ProfileProduct({ userData }) {
   const [productDatas, setProductDatas] = useState(null);
-  const params = useParams();
-  const accountname = params.id;
-  async function fetchUserProducts() {
-    const res = await fetch(`https://api.mandarin.weniv.co.kr/product/${accountname}/?limit=5`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-        "Content-type": "application/json",
-      },
-    });
-    const json = await res.json();
-    console.log(json);
-    const userProductItems = json.product;
-    setProductDatas(userProductItems);
-  }
+  const { id: accountname } = useParams();
 
   useEffect(() => {
-    fetchUserProducts();
-  }, [params]);
+    fetchApi(`product/${accountname}/?limit=5`).then((res) => setProductDatas(res.product));
+  }, [accountname]);
 
   return (
     userData &&

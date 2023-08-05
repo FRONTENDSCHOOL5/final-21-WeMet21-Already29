256ba63dc3094f3e0793cb9cd6cc75bda452124f
BK <3031011@naver.com>
2023-08-05 19:42:56 +0900

    refactor: hasMore 상태 통한 잔여 데이터 유무 확인 코드 추가

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -1,25 +1,48 @@
 import React, { useEffect, useState } from "react";
 
-export default function useInfiniteScroll(fetchPath, pageEnd) {
+const useInfiniteScroll = (fetchPath, pageEnd) => {
   const [page, setPage] = useState(0);
   const [isLoading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  console.log(page, hasMore);
 
+  // Infinite scroll 관찰자 설정 및 업데이트
   useEffect(() => {
-    if (isLoading) {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          if (entries[0].isIntersecting) {
-            setPage((prev) => prev + 1);
-          }
-        },
-        { threshold: 1 }
-      );
-      observer.observe(pageEnd.current);
-    }
-  }, [isLoading, pageEnd]);
+    // 관찰자 콜백: 리스트의 끝에 도달하면 새 데이터를 불러옴
+    const observerCallback = (entries) => {
+      if (entries[0].isIntersecting && hasMore) {
+        loadMoreData();
+      }
+    };
 
+    // 다음 페이지 데이터 불러오기
+    const loadMoreData = async () => {
+      if (!hasMore) return;
+
+      setLoading(true);
+      await getData(page);
+      setLoading(false);
+
+      if (hasMore) setPage((prev) => prev + 1);
+    };
+
+    if (pageEnd.current && !isLoading) {
+      const observer = new IntersectionObserver(observerCallback, { threshold: 1 });
+      observer.observe(pageEnd.current);
+
+      // 컴포넌트 정리 시 관찰 중지
+      return () => {
+        if (pageEnd.current) {
+          observer.unobserve(pageEnd.current);
+        }
+      };
+    }
+  }, [pageEnd.current, isLoading, hasMore, page]);
+
+  // 추후 응답으로 구분되는 데이터 가져오기
   const getData = async (page) => {
-    try {
+    // 데이터 조회 API 요청
+    const fetchData = async () => {
       const res = await fetch(`https://api.mandarin.weniv.co.kr/${fetchPath}/?limit=10&skip=${page * 10}`, {
         method: "GET",
         headers: {
@@ -27,14 +50,37 @@ export default function useInfiniteScroll(fetchPath, pageEnd) {
           "Content-type": "application/json",
         },
       });
-      setLoading(true);
-      const json = await res.json();
+      return res.json();
+    };
+
+    // JSON 응답에서 데이터를 추출
+    const extractData = (json) => {
+      const dataKeys = ["data", "posts", "comments", "post"];
+
+      for (const key of dataKeys) {
+        if (json[key]) {
+          return json[key];
+        }
+      }
+
+      return json.length;
+    };
+
+    try {
+      const json = await fetchData();
+      const data = extractData(json);
+
+      setHasMore(data >= 10);
+      setLoading(false);
+
       return json;
     } catch (e) {
       console.error(e);
+      setLoading(false);
     }
   };
 
-  // 반환 값 getData() : promise | page : 현재 페이지 | isLoading : 현재 로딩 상태
-  return { getData, page, setPage, isLoading };
-}
+  return { getData, page, setPage, hasMore };
+};
+
+export default useInfiniteScroll;

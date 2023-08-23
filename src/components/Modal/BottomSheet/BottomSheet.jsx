fc624a78c45b62bd676e9a6d7ec9474a260a177d
beomkwan.choi <3031011@naver.com>
2023-08-23 14:04:21 +0900

    refactor: 바텀시트 모달 키보드 접근성 준수

--- src/components/Modal/BottomSheet/BottomSheet.jsx
+++ src/components/Modal/BottomSheet/BottomSheet.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import BottomSheetContext from "../../../contexts/ModalContext/BottomSheetContext";
 import { BottomSheetBackDrop, BottomSheetWrapper } from "./BottomSheetStyle";
 
 export default function BottomSheet({ children }) {
+  const { isBottomSheetOpen, setBottomSheetOpen } = useContext(BottomSheetContext);
+  const bottomSheet = useRef(null);
+  useEffect(() => {
+    const firstEl = bottomSheet.current.firstChild,
+      lastEl = bottomSheet.current.lastChild;
+
+    firstEl.focus();
+
+    firstEl.addEventListener("keydown", (e) => {
+      if (e.key === "Tab" && e.shiftKey) {
+        e.preventDefault();
+        lastEl.focus();
+      }
+    });
+    lastEl.addEventListener("keydown", (e) => {
+      if (e.key === "Tab" && !e.shiftKey) {
+        e.preventDefault();
+        firstEl.focus();
+      }
+    });
+  }, [bottomSheet]);
   return (
     <>
-      <BottomSheetContext.Consumer>{({ setBottomSheetOpen }) => <BottomSheetBackDrop onClick={() => setBottomSheetOpen(false)} />}</BottomSheetContext.Consumer>
-      <BottomSheetWrapper>{children}</BottomSheetWrapper>
+      <BottomSheetBackDrop onClick={() => setBottomSheetOpen(false)} />
+      <BottomSheetWrapper ref={bottomSheet}>{children}</BottomSheetWrapper>
     </>
   );
 }
